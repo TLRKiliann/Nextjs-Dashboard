6a91954a3cb9f97e60652cb00b75ec3b69704bab
TLRKiliann <philogenie@protonmail.com>
2024-06-22 15:09:34 +0200

    my user for test added & some changes

--- my-app/app/lib/datadb.ts
+++ my-app/app/lib/datadb.ts
@@ -5,6 +5,7 @@ import justineImg from '@/public/assets/users/justine.jpg';
 import jasonImg from '@/public/assets/users/jason.png';
 import rebeccaImg from '@/public/assets/users/rebecca.jpg';
 import paulaImg from '@/public/assets/users/paula.jpg';
+import koalaImg from '@/public/assets/users/user_icon.jpg';
 import MariaImg from '@/public/assets/users/maria.jpg';
 
 export const customers: CustomersProps[] = [
@@ -14,6 +15,8 @@ export const customers: CustomersProps[] = [
         lastname: "Nier",
         img: bruceImg,
         spend: 44,
+        artQuantity: 9,
+        address: "not set",
         country: "Germany",
         city: "Berlin",
         connected: true
@@ -24,6 +27,8 @@ export const customers: CustomersProps[] = [
         lastname: "Twins",
         img: celestineImg,
         spend: 109,
+        artQuantity: 17,
+        address: "not set",
         country: "Japan",
         city: "Kasugai",
         connected: false
@@ -34,6 +39,8 @@ export const customers: CustomersProps[] = [
         lastname: "Romanoff",
         img: justineImg,
         spend: 232,
+        artQuantity: 23,
+        address: "not set",
         country: "Japan",
         city: "Narita",
         connected: true
@@ -44,6 +51,8 @@ export const customers: CustomersProps[] = [
         lastname: "Kent",
         img: jasonImg,
         spend: 232,
+        artQuantity: 77,
+        address: "not set",
         country: "USA",
         city: "Metropolis",
         connected: true
@@ -54,6 +63,8 @@ export const customers: CustomersProps[] = [
         lastname: "Miller",
         img: rebeccaImg,
         spend: 232,
+        artQuantity: 102,
+        address: "not set",
         country: "USA",
         city: "Gotham City",
         connected: false
@@ -64,16 +75,32 @@ export const customers: CustomersProps[] = [
         lastname: "Bell",
         img: paulaImg,
         spend: 232,
+        artQuantity: 231,
+        address: "Kungsbroplan 1",
         country: "Sweden",
         city: "Stockholm",
         connected: true
     },
     {
         id: 7,
+        username: "Esteban",
+        lastname: "Catanea",
+        img: koalaImg,
+        spend: 177,
+        artQuantity: 33,
+        address: "Spychertenstrasse 3",
+        country: "Switzerland",
+        city: "Thun",
+        connected: true
+    },
+    {
+        id: 8,
         username: "Maria",
         lastname: "Solengard",
         img: MariaImg,
-        spend: 232,
+        spend: 442,
+        artQuantity: 53,
+        address: "Vester Voldgade 4",
         country: "Denmark",
         city: "Copenhagen",
         connected: false
