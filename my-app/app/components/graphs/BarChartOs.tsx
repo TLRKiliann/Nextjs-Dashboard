245c86d7e6b26bee963378fde810e8cf9eaa9211
TLRKiliann <philogenie@protonmail.com>
2024-06-22 10:59:13 +0200

    os chart created

---
+++ my-app/app/components/graphs/BarChartOs.tsx
@@ -0,0 +1,74 @@
+"use client";
+//localhost:3000/dashboard/datatables
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  indexAxis: 'y' as const,
+  elements: {
+    bar: {
+      borderWidth: 2,
+    },
+  },
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'right' as const,
+    },
+    title: {
+      display: false,
+      text: 'Platform OS',
+    },
+  },
+};
+
+const labels = ['Window', 'Mac', 'Linux'];
+
+const dataset1Values = [300, 200, 100];
+const dataset2Values = [350, 250, 150];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Girls',
+      data: dataset1Values,
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Men',
+      data: dataset2Values,
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+const BarChart = () => {
+    return (
+        <div className="flex flex-col items-center justify-center w-full h-full">
+            <h2 className="text-base -mt-8 mb-2">Platform OS</h2>
+            <Bar options={options} data={data} className="w-full h-full px-2 -mb-10 rounded-lg" />
+        </div>
+    );
+}
+export default BarChart;
