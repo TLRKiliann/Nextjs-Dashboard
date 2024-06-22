4c3fb7677faff81679fa6d01d8a1660d8b90d01e
TLRKiliann <philogenie@protonmail.com>
2024-06-22 18:40:34 +0200

    code review and styles improved

--- my-app/app/components/menu-items/Profile.tsx
+++ my-app/app/components/menu-items/Profile.tsx
@@ -3,6 +3,7 @@
 import { CustomersProps } from '@/app/lib/definitions';
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
+import DataProfile from './data-profile';
 
 const isBrowser = () => typeof window !== 'undefined';
 
@@ -19,106 +20,55 @@ export default function Profile({customers}: {customers: CustomersProps[]}) {
             setOssystem((prev) => prev = window.navigator.userAgent);
             //const ip = https://jsonip.com/);
         };
+        return () => console.log("clean-up");
     }, []);
 
     return (
         <div className='flex flex-col justify-center w-full h-full'>
             {customers.map((customer: CustomersProps) => customer.username === userSession ? (
-                <div key={customer.id} className='flex flex-col items-start justify-center p-4'>
+                <div key={customer.id} className='flex flex-col items-start justify-center mx-4'>
 
-                    <div className='flex flex-row items-center justify-between w-full h-auto'>
-                        
-                        <div className='flex flex-col items-start justify-center w-full h-full'>
-                            
-                            <div className='w-full flex flex-row items-center justify-start mb-2%'>
-                                <p className="w-2/5">
-                                    Username: 
-                                </p>
-                                <p className="w-auto -ml-1">
-                                    {customer.username}
-                                </p>
-                            </div>
-                            
-                            <div className='w-full flex flex-row items-center justify-start mb-2%'>
-                                <p className="w-2/5">
-                                    Lastname: 
-                                </p>
-                                <p className="w-auto -ml-1">
-                                    {customer.lastname}
-                                </p>
-                            </div>
-
-                        </div>
-
-                        <div className='w-1/5 border'>
-                            <Image src={customer.img} width={400} height={250} alt="no img" 
-                                className='object-fit'
-                            />
-                        </div>
+                    <div className='relative w-full flex justify-end bg-slate-100'>
+                        <Image src={customer.img} width={400} height={250} alt="no img" 
+                            className='md:w-[70px] xl:w-[140px] h-auto object-fit rounded-bl-md shadow-md'
+                        />
                     </div>
 
-                    <div className='w-4/5 flex flex-row items-center justify-start mb-2%'>
-                        <p className="w-2/5">
-                            Address: 
-                        </p>
-                        <p className="w-auto md:text-base xl:text-lg">
-                            {customer.address}
-                        </p>
-                    </div>
+                    <DataProfile varDef="Username:">
+                        {customer.username}
+                    </DataProfile>
+                    
+                    <DataProfile varDef="Lastname:">
+                        {customer.lastname}
+                    </DataProfile>
 
-                    <div className='w-4/5 flex flex-row items-center justify-start mb-2%'>
-                        <p className="w-2/5">
-                            City: 
-                        </p>
-                        <p className="w-auto md:text-base xl:text-lg">
-                            {customer.city}
-                        </p>
-                    </div>
+                    <DataProfile varDef="Address:">
+                        {customer.address}
+                    </DataProfile>
 
-                    <div className='w-4/5 flex flex-row items-center justify-start mb-2%'>
-                        <p className="w-2/5">
-                            Country: 
-                        </p>
-                        <p className="w-auto md:text-base xl:text-lg">
-                            {customer.country}
-                        </p>
-                    </div>
+                    <DataProfile varDef="City:">
+                        {customer.city}
+                    </DataProfile>
 
-                    <div className='w-4/5 flex flex-row items-center justify-start mb-2%'>
-                        <p className="w-2/5">
-                            Spend: 
-                        </p>
-                        <p className="w-auto md:text-base xl:text-lg">
-                            {customer.spend}
-                        </p>
-                    </div>
+                    <DataProfile varDef="Country:">
+                        {customer.country}
+                    </DataProfile>
 
-                    <div className='w-4/5 flex flex-row items-center justify-start mb-2%'>
-                        <p className="w-2/5">
-                            Articles (quantity): 
-                        </p>
-                        <p className="w-auto md:text-base xl:text-lg">
-                            {customer.artQuantity}
-                        </p>
-                    </div>
+                    <DataProfile varDef="Spend:">
+                        {customer.spend}.-
+                    </DataProfile>
 
-                    <div className='w-4/5 flex flex-row items-center justify-start mb-2%'>
-                        <p className="w-2/5">
-                            Browser: 
-                        </p>
-                        <p className="w-auto md:text-base xl:text-lg">
-                            {browser?.slice(57, 70)}
-                        </p>
-                    </div>
+                    <DataProfile varDef="Articles:">
+                        {customer.artQuantity}
+                    </DataProfile>
 
-                    <div className='w-4/5 flex flex-row items-center justify-start mb-2%'>
-                        <p className="w-2/5">
-                            OS: 
-                        </p>
-                        <p className="w-auto md:text-base xl:text-lg">
-                            {ossystem?.slice(18, 30)}
-                        </p>
-                    </div>
+                    <DataProfile varDef="Browser:">
+                        {browser?.slice(57, 70)}
+                    </DataProfile>
+
+                    <DataProfile varDef="Os:">
+                        {ossystem?.slice(18, 30)}
+                    </DataProfile>
 
                 </div>
             ) : null)}
