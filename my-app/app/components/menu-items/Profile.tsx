04f963e45a5c35b0d656acb7efe85e88fb3e5335
TLRKiliann <philogenie@protonmail.com>
2024-06-22 17:32:45 +0200

    style of profile improved

--- my-app/app/components/menu-items/Profile.tsx
+++ my-app/app/components/menu-items/Profile.tsx
@@ -1,26 +1,125 @@
+"use client";
+
 import { CustomersProps } from '@/app/lib/definitions';
-import React from 'react';
-//import { customers } from '@/app/lib/datadb';
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+
+const isBrowser = () => typeof window !== 'undefined';
 
 export default function Profile({customers}: {customers: CustomersProps[]}) {
     const userSession: string = "Esteban";
-    const browser: string = window.navigator.userAgent;
-    const ossystem: string = window.navigator.userAgent;
-    //const ip = https://jsonip.com/);
+
+    const [browser, setBrowser] = useState<string | null>(null);
+    const [ossystem, setOssystem] = useState<string | null>(null);
+
+    //to avoid ssr error
+    useEffect(() => {
+        if (isBrowser()) {
+            setBrowser((prev) => prev = window.navigator.userAgent);
+            setOssystem((prev) => prev = window.navigator.userAgent);
+            //const ip = https://jsonip.com/);
+        };
+    }, []);
 
     return (
         <div className='flex flex-col justify-center w-full h-full'>
             {customers.map((customer: CustomersProps) => customer.username === userSession ? (
                 <div key={customer.id} className='flex flex-col items-start justify-center p-4'>
-                    <p>Username: {customer.username}</p>
-                    <p>Lastname: {customer.lastname}</p>
-                    <p>Address: {customer.address}</p>
-                    <p>City: {customer.city}</p>
-                    <p>Country: {customer.country}</p>
-                    <p>Sepend: {customer.spend}</p>
-                    <p>Articles Quantity: {customer.artQuantity}</p>
-                    <p>Browser: {browser}</p>
-                    <p>OS: {ossystem}</p>
+
+                    <div className='flex flex-row items-center justify-between w-full h-auto'>
+                        
+                        <div className='flex flex-col items-start justify-center w-full h-full'>
+                            
+                            <div className='w-full flex flex-row items-center justify-start mb-2%'>
+                                <p className="w-2/5">
+                                    Username: 
+                                </p>
+                                <p className="w-auto -ml-1">
+                                    {customer.username}
+                                </p>
+                            </div>
+                            
+                            <div className='w-full flex flex-row items-center justify-start mb-2%'>
+                                <p className="w-2/5">
+                                    Lastname: 
+                                </p>
+                                <p className="w-auto -ml-1">
+                                    {customer.lastname}
+                                </p>
+                            </div>
+
+                        </div>
+
+                        <div className='w-1/5 border'>
+                            <Image src={customer.img} width={400} height={250} alt="no img" 
+                                className='object-fit'
+                            />
+                        </div>
+                    </div>
+
+                    <div className='w-4/5 flex flex-row items-center justify-start mb-2%'>
+                        <p className="w-2/5">
+                            Address: 
+                        </p>
+                        <p className="w-auto md:text-base xl:text-lg">
+                            {customer.address}
+                        </p>
+                    </div>
+
+                    <div className='w-4/5 flex flex-row items-center justify-start mb-2%'>
+                        <p className="w-2/5">
+                            City: 
+                        </p>
+                        <p className="w-auto md:text-base xl:text-lg">
+                            {customer.city}
+                        </p>
+                    </div>
+
+                    <div className='w-4/5 flex flex-row items-center justify-start mb-2%'>
+                        <p className="w-2/5">
+                            Country: 
+                        </p>
+                        <p className="w-auto md:text-base xl:text-lg">
+                            {customer.country}
+                        </p>
+                    </div>
+
+                    <div className='w-4/5 flex flex-row items-center justify-start mb-2%'>
+                        <p className="w-2/5">
+                            Spend: 
+                        </p>
+                        <p className="w-auto md:text-base xl:text-lg">
+                            {customer.spend}
+                        </p>
+                    </div>
+
+                    <div className='w-4/5 flex flex-row items-center justify-start mb-2%'>
+                        <p className="w-2/5">
+                            Articles (quantity): 
+                        </p>
+                        <p className="w-auto md:text-base xl:text-lg">
+                            {customer.artQuantity}
+                        </p>
+                    </div>
+
+                    <div className='w-4/5 flex flex-row items-center justify-start mb-2%'>
+                        <p className="w-2/5">
+                            Browser: 
+                        </p>
+                        <p className="w-auto md:text-base xl:text-lg">
+                            {browser?.slice(57, 70)}
+                        </p>
+                    </div>
+
+                    <div className='w-4/5 flex flex-row items-center justify-start mb-2%'>
+                        <p className="w-2/5">
+                            OS: 
+                        </p>
+                        <p className="w-auto md:text-base xl:text-lg">
+                            {ossystem?.slice(18, 30)}
+                        </p>
+                    </div>
+
                 </div>
             ) : null)}
         </div>
--- my-app/app/dashboard/dashboardnative/@tableone/profile/page.tsx
+++ my-app/app/dashboard/dashboardnative/@tableone/profile/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from 'react';
 import { useRouter } from 'next/navigation';
 import TablePage from '@/app/components/TablePage';
