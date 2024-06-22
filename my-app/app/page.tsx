896d3d5724f3999f94ab09d1b56dbb05e22ed540
TLRKiliann <philogenie@protonmail.com>
2024-06-22 15:40:45 +0200

    some changes

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Dashboard- Project</h1>
