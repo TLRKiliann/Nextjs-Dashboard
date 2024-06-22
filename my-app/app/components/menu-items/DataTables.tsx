bcd691379d891e826be430422dacac4eb9239eb2
TLRKiliann <philogenie@protonmail.com>
2024-06-22 11:04:31 +0200

    code review & clean code

--- my-app/app/components/menu-items/DataTables.tsx
+++ my-app/app/components/menu-items/DataTables.tsx
@@ -1,48 +1,34 @@
 import React from 'react';
 import TableGraphs from '../TableGraphs';
 import LineChart from '../graphs/LineChart';
+import BarChartOs from '../graphs/BarChartOs';
+import BarChartBrowser from '../graphs/BarChartBrowser';
 
 export default function DataTables() {
     return (
         <div className='w-full h-full grid grid-cols-2 grid-rows-2'>
 
             <TableGraphs>
-                <div className='flex items-center h-[10%] mt-4 mb-0 ml-4'>
-                    <h2 className='text-xl'>Connection Per Day</h2>
-                </div>
-
-                <div className='h-[90%]'>
+                <div className='flex items-center justify-center h-[100%]'>
                     <LineChart />
                 </div>
             </TableGraphs>
 
             <TableGraphs>
-                <div className='flex items-center h-[10%] mt-4 mb-0 ml-4'>
-                    <h2 className='text-xl'>Daily Traffic</h2>
-                </div>
-
-                <div className='h-[90%]'>
+                <div className='flex items-center justify-center h-[100%]'>
                     <LineChart />
                 </div>
             </TableGraphs>
 
             <TableGraphs>
-                <div className='flex items-center h-[10%] mt-4 mb-0 ml-4'>
-                    <h2 className='text-xl'>Amount Spend</h2>
-                </div>
-
-                <div className='h-[90%]'>
-                    <LineChart />
+                <div className='flex items-center justify-center h-[100%]'>
+                    <BarChartOs />
                 </div>
             </TableGraphs>
 
             <TableGraphs>
-                <div className='flex items-center h-[10%] mt-4 mb-0 ml-4'>
-                    <h2 className='text-xl'>Stock</h2>
-                </div>
-
-                <div className='h-[90%]'>
-                    <LineChart />
+                <div className='flex items-center justify-center h-[100%]'>
+                    <BarChartBrowser />
                 </div>
             </TableGraphs>
 
