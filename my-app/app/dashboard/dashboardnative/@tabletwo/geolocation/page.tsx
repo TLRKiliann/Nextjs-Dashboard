7e01de0e29be0e0d4247bbe61d0b5319bdff40a7
TLRKiliann <philogenie@protonmail.com>
2024-06-23 17:08:01 +0200

    raw query to call api setup

--- my-app/app/dashboard/dashboardnative/@tabletwo/geolocation/page.tsx
+++ my-app/app/dashboard/dashboardnative/@tabletwo/geolocation/page.tsx
@@ -1,66 +1,26 @@
-"use client";
 //http://localhost:3000/dashboard/dashboardnative/geolocation
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+
+import React from 'react';
 import TablePage from '@/app/components/TablePage';
 import MapChart from '@/app/components/graphs/MapChart';
+import { ApiPublicIp, ApiGeolocation } from '@/app/utils/api-request';
+import Link from 'next/link';
 
-const isGeoLocation = () => typeof window !== 'undefined';
+export default async function GeolocationPage() {
 
-export default function GeolocationPage() {
+    const ipResult = await ApiPublicIp();
+    if (!ipResult) {
+        console.error('Error fetching IP');
+    } else {
+        console.log('Public IP:', ipResult);
+    }
 
-    const router = useRouter();
-
-    //Ref error: window is undefined !!!
-    
-    //const [geoLocation, setGeoLocation] = useState<string | undefined>(undefined);
-    const [location, setLocation] = useState<{latitude: number, longitude: number} | null>(null);
-    const [errorMessage, setErrorMessage] = useState('');
-
-    useEffect(() => {
-        if (isGeoLocation()) {
-            window.navigator.permissions.query({ name: 'geolocation' }).then((result) => {
-                if (result.state === 'granted') {
-                    getCurrentLocation();
-                } else if (result.state === 'prompt') {
-                    getCurrentLocation();
-                } else if (result.state === 'denied') {
-                    setErrorMessage('Geolocation permission denied. Please enable location services for this site in your browser settings.');
-                }
-            });
-        } else {
-            setErrorMessage('Geolocation is not supported by this browser or not running in the browser environment.');
-        }
-        return () => {
-            console.log("Clean-up");
-        };
-    }, []);
-
-    const getCurrentLocation = () => {
-        if (isGeoLocation()) {
-            window.navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    let latitude: number = position.coords.latitude;
-                    let longitude: number = position.coords.longitude;
-                    setLocation({ latitude, longitude });
-                    console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-                },
-                (error) => {
-                    if (error.code === 1) {
-                        setErrorMessage('User denied geolocation prompt');
-                    } else {
-                        console.error('Error getting geolocation:', error);
-                    }
-                }
-            );
-        } else {
-            console.log("window error: undefined !");
-        }
-    };
-
-    console.log(location?.latitude);
-    console.log(location?.longitude);
-    console.log(errorMessage)
+    const geoResult = await ApiGeolocation(ipResult);
+    if (!geoResult) {
+        console.error('Error fetching IP');
+    } else {
+        console.log('geoResult:', geoResult);
+    }
 
     return (
         <TablePage>
@@ -69,14 +29,14 @@ export default function GeolocationPage() {
             </div>
 
             <div className='w-full h-[80%]'>
-                <MapChart />
+                <MapChart latitude={geoResult.latitude} longitude={geoResult.longitude} />
             </div>
 
             <div className='flex items-end justify-end h-[10%] border'>
-                <button type="button" onClick={() => router.back()} 
+                <Link href="/dashboard/dashboardnative"
                     className='text-sm text-blue-400 hover:text-blue-500 active:text-blue-700'>
-                    Table 2
-                </button>
+                    Table 2 ?
+                </Link>
             </div>
         </TablePage>
     )
