61ea03dc9a947de4bf9fcdad4987dd626ec605a5
TLRKiliann <philogenie@protonmail.com>
2024-06-24 16:58:56 +0200

    more responsive graph

--- my-app/app/dashboard/dashboardnative/@tabletwo/page.tsx
+++ my-app/app/dashboard/dashboardnative/@tabletwo/page.tsx
@@ -6,16 +6,16 @@ import BestSellersChart from '@/app/components/graphs/BestSellersChart';
 export default function TableTwoPage() {
     return (
         <TablePage>
-            <div className='h-[10%]'>
+            <div className='h-[20%]'>
                 <h2 className='text-xl'>Best Sellers</h2>
             </div>
 
-            <div className='flex flex-col items-center justify-center w-auto h-[80%] bg-slate-100 
+            <div className='flex flex-col items-center justify-center w-[90%] h-[60%] m-auto bg-slate-100 
                 rounded-lg shadow-sm-out'>
                 <BestSellersChart />
             </div>
 
-            <div className='flex items-end justify-end h-[10%]'>
+            <div className='flex items-end justify-end h-[20%]'>
                 <li className='list-none text-sm text-blue-400 hover:text-blue-500 active:text-blue-700'>
                     <Link href="/dashboard/dashboardnative/geolocation">Geolocation</Link>
                 </li>
