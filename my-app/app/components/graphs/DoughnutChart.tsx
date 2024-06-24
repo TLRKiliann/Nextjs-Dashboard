ad9e30cf6d3e7a67981071d0980b31190b5b518e
TLRKiliann <philogenie@protonmail.com>
2024-06-24 17:36:04 +0200

    doughnut created

---
+++ my-app/app/components/graphs/DoughnutChart.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+        position: 'right' as const,
+        },
+        title: {
+        display: false,
+        text: 'Doughnut Chart',
+        },
+    },
+};
+
+export const data = {
+  labels: ['Unsatisfied', 'View', 'No Comment', 'Satisfied'],
+  datasets: [
+    {
+        label: '# of Votes',
+        data: [16, 10, 12, 20],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.5)',
+            'rgba(54, 162, 235, 0.5)',
+            'rgba(255, 206, 86, 0.5)',
+            'rgba(75, 192, 192, 0.5)',
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+        ],
+        borderWidth: 1,
+    }],
+};
+
+export function DoughnutChart() {
+    return (
+        <div className='flex flex-col items-center justify-center w-full h-[100%] text-slate-300 rounded-lg'>
+            <h2 className='text-base mt-10'>Satisfaction</h2>
+            <Doughnut options={options} data={data} className='w-auto h-[100%] -mt-10 rounded-lg'/> 
+        </div>
+    )
+}
