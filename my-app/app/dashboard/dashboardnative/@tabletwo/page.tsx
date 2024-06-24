8e3c8ab772e53333b397e55cfe9f7aedfc3c6fa0
TLRKiliann <philogenie@protonmail.com>
2024-06-24 15:54:27 +0200

    style improve with box-shadow

--- my-app/app/dashboard/dashboardnative/@tabletwo/page.tsx
+++ my-app/app/dashboard/dashboardnative/@tabletwo/page.tsx
@@ -6,16 +6,16 @@ import BestSellersChart from '@/app/components/graphs/BestSellersChart';
 export default function TableTwoPage() {
     return (
         <TablePage>
-            <div className='h-[10%] border'>
+            <div className='h-[10%]'>
                 <h2 className='text-xl'>Best Sellers</h2>
             </div>
 
             <div className='flex flex-col items-center justify-center w-auto h-[80%] bg-slate-100 
-                rounded-lg shadow-in p-2'>
+                rounded-lg shadow-sm-out'>
                 <BestSellersChart />
             </div>
 
-            <div className='flex items-end justify-end h-[10%] border'>
+            <div className='flex items-end justify-end h-[10%]'>
                 <li className='list-none text-sm text-blue-400 hover:text-blue-500 active:text-blue-700'>
                     <Link href="/dashboard/dashboardnative/geolocation">Geolocation</Link>
                 </li>
