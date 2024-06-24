9b4b88ba87317902f62f51a39fbf48a316616d7f
TLRKiliann <philogenie@protonmail.com>
2024-06-24 17:36:56 +0200

    some change for doughnut

--- my-app/app/components/TableGraphs.tsx
+++ my-app/app/components/TableGraphs.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function TableGraphs({children}: {children: React.ReactNode}) {
     return (
-        <div className='bg-slate-900 rounded-lg shadow-xl m-4'>
+        <div className='bg-slate-800 rounded-lg shadow-xl m-4'>
             {children}
         </div>
     )
