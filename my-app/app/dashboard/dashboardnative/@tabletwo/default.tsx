9f694e694f01f6d390a5d4a4696a513ca13290c4
TLRKiliann <philogenie@protonmail.com>
2024-06-24 13:54:43 +0200

    copy from page.tsx done

--- my-app/app/dashboard/dashboardnative/@tabletwo/default.tsx
+++ my-app/app/dashboard/dashboardnative/@tabletwo/default.tsx
@@ -1,31 +1,19 @@
 import React from 'react';
 import Link from 'next/link';
 import TablePage from '@/app/components/TablePage';
+import BestSellersChart from '@/app/components/graphs/BestSellersChart';
 
 export default function TableTwoDefault() {
     return (
         <TablePage>
             <div className='h-[10%] border'>
-                <h2 className='text-xl'>second 2</h2>
+                <h2 className='text-xl'>Best Sellers</h2>
             </div>
 
-            <ul className='flex flex-col items-center justify-center w-full h-[80%] border'>
-                <li className='w-full bg-blue-300 my-2 border rounded-lg'>
-                    <div className='flex items-center justify-between h-[40px] px-4 border'>
-                        <p>Earnings</p><p>icons</p>
-                    </div>
-                </li>
-                <li className='w-full bg-pink-300 my-2 border rounded-lg'>
-                    <div className='flex items-center justify-between h-[40px] px-4 border'>
-                        <p>Spend this month</p><p>icons</p>
-                    </div>
-                </li>
-                <li className='w-full bg-emerald-300 my-2 border rounded-lg'>
-                    <div className='flex items-center justify-between h-[40px] px-4 border'>
-                        <p>Sales</p><p>icons</p>
-                    </div>
-                </li>
-            </ul>
+            <div className='flex flex-col items-center justify-center w-auto h-[80%] bg-slate-100 
+                rounded-lg shadow-in p-2'>
+                <BestSellersChart />
+            </div>
 
             <div className='flex items-end justify-end h-[10%] border'>
                 <li className='list-none text-sm text-blue-400 hover:text-blue-500 active:text-blue-700'>
