bbae065884dd94e628d86446ea7abd63b4443487
TLRKiliann <philogenie@protonmail.com>
2024-06-24 10:47:20 +0200

    React.Fragment replaced by empty div tag

--- my-app/app/dashboard/dashboardnative/page.tsx
+++ my-app/app/dashboard/dashboardnative/page.tsx
@@ -4,8 +4,8 @@ import Menu from '@/app/components/Menu';
 
 export default function DashboardPage() {
     return (
-        <React.Fragment>
-            <div className='fixed right-0 z-10 flex flex-row items-center w-[80%] h-[10vh] 
+        <>
+            <div className='absolute right-0 z-10 flex flex-row items-center w-[80%] h-[10vh] 
                 bg-slate-200 border-b border-slate-300/20 shadow-md'>
                 <Header />
             </div>
@@ -13,6 +13,6 @@ export default function DashboardPage() {
             <div className='flex-none w-[20%] h-full bg-slate-800'>
                 <Menu />
             </div>
-        </React.Fragment>
+        </>
     )
 };
