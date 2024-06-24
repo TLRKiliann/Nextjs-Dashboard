4ab7af54ed2d7a384714b7f6054fc90853b706e3
TLRKiliann <philogenie@protonmail.com>
2024-06-24 21:31:54 +0200

    add scale

--- my-app/app/components/TableGraphs.tsx
+++ my-app/app/components/TableGraphs.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 export default function TableGraphs({children}: {children: React.ReactNode}) {
     return (
-        <div className='bg-slate-800 rounded-lg shadow-xl m-4'>
+        <div className='bg-slate-900/90 transform duration-300 ease-in-out hover:scale-110
+            rounded-lg shadow-out m-4'>
             {children}
         </div>
     )
