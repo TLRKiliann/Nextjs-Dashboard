d91276dbeb57b0f9c1fb4e7d8a9ed175864be2f9
TLRKiliann <philogenie@protonmail.com>
2024-06-25 11:26:21 +0200

    size of tables-graph adapted for xl format screen

--- my-app/app/components/menu-items/DataTables.tsx
+++ my-app/app/components/menu-items/DataTables.tsx
@@ -3,32 +3,32 @@ import TableGraphs from '../TableGraphs';
 import LineChart from '../graphs/LineChart';
 import BarChartOs from '../graphs/BarChartOs';
 import BarChartBrowser from '../graphs/BarChartBrowser';
-import {DoughnutChart} from '../graphs/DoughnutChart';
+import DoughnutChart from '../graphs/DoughnutChart';
 
 export default function DataTables() {
     return (
-        <div className='w-full h-full grid grid-cols-2 grid-rows-2'>
+        <div className='w-full h-full grid grid-cols-2 grid-rows-2 gap-4'>
 
             <TableGraphs>
-                <div className='flex items-center justify-center h-[100%]'>
+                <div className='flex items-center justify-center w-full h-full'>
                     <LineChart />
                 </div>
             </TableGraphs>
 
             <TableGraphs>
-                <div className='flex items-center justify-center h-[100%]'>
+                <div className='flex items-center justify-center w-full h-full'>
                     <DoughnutChart />
                 </div>
             </TableGraphs>
 
             <TableGraphs>
-                <div className='flex items-center justify-center h-[100%]'>
+                <div className='flex items-center justify-center w-full h-full'>
                     <BarChartOs />
                 </div>
             </TableGraphs>
 
             <TableGraphs>
-                <div className='flex items-center justify-center h-[100%]'>
+                <div className='flex items-center justify-center w-full h-full'>
                     <BarChartBrowser />
                 </div>
             </TableGraphs>
