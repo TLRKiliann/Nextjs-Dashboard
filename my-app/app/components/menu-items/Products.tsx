526a76229d2551d4b0d1e49a2d8f92cbe180eaf5
TLRKiliann <philogenie@protonmail.com>
2024-06-25 22:33:48 +0200

    simulation of products finished

--- my-app/app/components/menu-items/Products.tsx
+++ my-app/app/components/menu-items/Products.tsx
@@ -5,23 +5,74 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import { products } from '@/app/lib/products';
 
+type AllStateProps = {
+    name: string;
+    family: string;
+    stock: string;
+    price: string;
+}
+
 export default function Products() {
 
     const [listProducts, setListProducts] = useState<ProductsProps[]>(products);
-
-    const handleAdd = (id: number) => {
+    const [allState, setAllState] = useState<AllStateProps>(
+        {
+            name: "",
+            family: "",
+            stock: "",
+            price: ""
+        }
+    );
+    /* const handleAdd = (id: number) => {
         const findById: ProductsProps[] = listProducts.map((list: ProductsProps) => list.id === id 
             ? {...list, stock: list.stock - 1, quantity: list.quantity + 1} : list);
         setListProducts(findById);
-    };
-
+    }; */
+    
     /* const handleDelete = (id: number) => {
         const findById: ProductsProps[] = listProducts.map((list: ProductsProps) => list.id === id 
             ? {...list, stock: list.stock + 1, quantity: list.quantity - 1} : list);
         setListProducts(findById);
     }; */
 
-    const handleDelete = (id: number) => {
+    const handleFamily = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setAllState((prev) => ({...prev, family: e.target.value}));
+    };
+
+    const handleName = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setAllState((prev) => ({...prev, name: e.target.value}));
+    };
+
+    const handleStock = (e: React.ChangeEvent<HTMLInputElement>) => {
+        //setAllState((prev) => ({...prev, stock: prev.stock + 1}));
+        setAllState((prev) => ({...prev, stock: e.target.value}));
+    };
+
+    const handlePrice = (e: React.ChangeEvent<HTMLInputElement>) => {
+        //setAllState((prev) => ({...prev, price: prev.price + 1}));
+        setAllState((prev) => ({...prev, price: e.target.value}));
+    };
+    
+    const handleModify = (id: number) => {
+        const modifyById = listProducts.map((list: ProductsProps) => list.id === id 
+            ? {...list, switch: !list.switch} : list);
+        setListProducts(modifyById);
+    };
+
+    //derivated state
+    const name: string = allState.name;
+    const family: string = allState.family;
+    const stock: string = allState.stock;
+    const price: string = allState.price;
+
+    const handleSave = (id: number): void => {
+        const modifyById = listProducts.map((list: ProductsProps) => list.id === id 
+            ? {...list, id: list.id, img: list.img, family: family, name: name, 
+                stock: Number(stock), price: Number(price), quantity: list.quantity, switch: !list.switch} : list);
+        setListProducts(modifyById);
+    };
+
+    const handleDelete = (id: number): void => {
         const findById: ProductsProps[] = listProducts.filter((list: ProductsProps) => list.id !== id);
         setListProducts(findById);
     };
@@ -34,7 +85,7 @@ export default function Products() {
 
                 {listProducts.map((product: ProductsProps) => (
                     <div key={product.id} 
-                        className='flex flex-row items-center justify-between w-full h-[80px] bg-slate-50 
+                        className='flex flex-row items-center justify-between w-full h-[80px] bg-white 
                             rounded-lg shadow-sm-out p-2 m-2'>
 
                         <div className='w-[60px] h-[60px]'>
@@ -47,28 +98,71 @@ export default function Products() {
                             />
                         </div>
 
-                        <div className='flex flex-row font-bold'>
-                            <h3>{product.family}</h3>
-                            <h4>&nbsp;{product.name}</h4>
-                        </div>
+                        {product.switch === false ? (
+                            <>
+                                <div className='w-1/5 flex flex-row items-center justify-center font-bold'>
+                                    <h3>{product.family}</h3>
+                                    <h4>&nbsp;{product.name}</h4>
+                                </div>
 
-                        <div className='flex flex-col'>
-                            <p>Stock: {product.stock}</p>
-                            <p className='font-bold'>Price: {product.price}.-</p>
-                        </div>
+                                <div className='w-2/5 flex flex-row items-center justify-around'>
+                                    <p>Stock: {product.stock}</p>
+                                    <p className='font-bold'>Price: {product.price}.-</p>
+                                </div>
+                            </>
+                        ) : (
+                            <>
+                                <div className='w-1/5 flex flex-row items-center justify-between font-bold'>
+                                    <input type="text" value={allState.family} onChange={(e)=> handleFamily(e)} 
+                                        placeholder={product.family} 
+                                        className='w-2/5 bg-slate-50 border border-slate-500 outline-blue-500 
+                                            focus:border-blue-500 focus:outline-none rounded-lg px-2 py-1'
+                                    />
+                                    <input type="text" value={allState.name} onChange={(e)=> handleName(e)} 
+                                        placeholder={product.name} 
+                                        className='w-2/5 bg-slate-50 border border-slate-500 outline-blue-500 
+                                            focus:border-blue-500 focus:outline-none rounded-lg px-2 py-1'
+                                    />
+                                </div>
 
-                        <p>Quantity: {product.quantity}</p>
+                                <div className='w-1/5 flex flex-row items-center justify-between'>
+                                    <input type="text" value={allState.stock} onChange={(e) => handleStock(e)} 
+                                        placeholder={String(product.stock)}
+                                        className='w-2/5 bg-slate-50 border border-slate-500 outline-blue-500 
+                                            focus:border-blue-500 focus:outline-none rounded-lg px-2 py-1'
+                                    />
+                                    <input type="text" value={allState.price} onChange={(e) => handlePrice(e)} 
+                                        placeholder={String(product.price)} 
+                                        className='w-2/5 bg-slate-50 border border-slate-500 outline-blue-500 
+                                            focus:border-blue-500 focus:outline-none rounded-lg px-2 py-1'
+                                    />
+                                </div>
+                            </>
+                        )}
 
-                        <div className='flex items-center justify-between'>
+                        <p className='text-center w-1/5'>Quantity: {product.quantity}</p>
+
+                        <div className='w-1/5 flex items-center justify-between'>
                             
-                            <button type="button" 
-                                onClick={() => handleAdd(product.id)}
-                                className='text-slate-100 bg-blue-500 
-                                hover:bg-blue-600 active:bg-blue-700
-                                px-4 py-1 rounded shadow-sm-out'
-                            >
-                                Add
-                            </button>
+                            {product.switch === false ? (
+                                <button type="button" 
+                                    onClick={() => handleModify(product.id)}
+                                    className='text-slate-100 bg-blue-500 
+                                    hover:bg-blue-600 active:bg-blue-700
+                                    px-4 py-1 rounded shadow-sm-out'
+                                >
+                                    Modify
+                                </button>
+                            ) : (
+                                <button type="button" 
+                                    onClick={() => handleSave(product.id)}
+                                    className='text-slate-100 bg-blue-500 
+                                    hover:bg-blue-600 active:bg-blue-700
+                                    px-4 py-1 rounded shadow-sm-out'
+                                >
+                                    Save
+                                </button>
+                            )}
 
                             <button type="button" 
                                 onClick={() => handleDelete(product.id)}
@@ -83,7 +177,6 @@ export default function Products() {
 
                     </div>
                 ))}
-
             </div>
         </div>
     )
