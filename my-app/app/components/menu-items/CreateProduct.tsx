2bb159459ce4729eb5f7d92a580b82a5b37c40e8
TLRKiliann <philogenie@protonmail.com>
2024-06-25 22:33:19 +0200

    resize all for a better UI

--- my-app/app/components/menu-items/CreateProduct.tsx
+++ my-app/app/components/menu-items/CreateProduct.tsx
@@ -4,39 +4,39 @@ export default function CreateProduct() {
     return (
         <div className='flex flex-col justify-center h-[40%] bg-slate-200'>
 
-            <div className='h-1/5 px-4'>
-                <h2 className='text-lg m-auto'>Create</h2>
+            <div className='h-1/5 p-4'>
+                <h2 className='text-lg m-auto'>Create Product</h2>
             </div>
 
-            <div className='h-4/5 flex flex-col items-center justify-center'>
+            <div className='h-4/5 flex flex-col items-center justify-center border border-slate-900 m-4'>
 
-                <form action="" className='w-[400px] h-full rounded-lg p-4'>
+                <form action="" className='w-[500px] h-full rounded-lg'>
                     
                     <div className='w-full flex flex-row items-start justify-between mb-3'>
-                        <label htmlFor="name">Name of product</label>
-                        <input type="text" id="name" name="name" />
+                        <label htmlFor="name">Name of product:</label>
+                        <input type="text" id="name" name="name" className='px-2 py-1'/>
                     </div>
 
                     <div className='w-full flex flex-row items-start justify-between mb-3'>
-                        <label htmlFor="img">Img of product</label>
-                        <input type="text" id="img" name="img" />
+                        <label htmlFor="img">Img of product:</label>
+                        <input type="text" id="img" name="img" className='px-2 py-1'/>
                     </div>
 
                     <div className='w-full flex flex-row items-start justify-between mb-3'>
-                        <label htmlFor="stock">Stock of product</label>
-                        <input type="number" id="stock" name="stock" />
+                        <label htmlFor="stock">Stock of product:</label>
+                        <input type="text" id="stock" name="stock" className='px-2 py-1'/>
                     </div>
 
                     <div className='w-full flex flex-row items-start justify-between mb-3'>
-                        <label htmlFor="price">Price of product</label>
-                        <input type="number" id="price" name="price" />
+                        <label htmlFor="price">Price of product:</label>
+                        <input type="text" id="price" name="price" className='px-2 py-1'/>
                     </div>
 
                     <div className='flex items-center justify-center mt-4'>
                         <button type="button"
                             className='text-slate-100 bg-blue-500 hover:bg-blue-600 active:bg-blue-700 rounded px-4 py-1'
                         >
-                            Create Product
+                            Validate
                         </button>
                     </div>
 
