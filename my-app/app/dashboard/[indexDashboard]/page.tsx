967568bc61d14771942e31eb82449d10274af6c9
TLRKiliann <philogenie@protonmail.com>
2024-06-26 10:31:27 +0200

    Suspense added with a loader in fallback

--- my-app/app/dashboard/[indexDashboard]/page.tsx
+++ my-app/app/dashboard/[indexDashboard]/page.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import { Metadata } from 'next';
+import React, { Suspense } from 'react';
 import Header from '@/app/components/Header';
 import Menu from '@/app/components/Menu';
+import Loader from '@/app/components/Loader';
+
+export const metadata: Metadata = {
+    title: "Dashboard (index)",
+    description: "access accepted"
+};
 
 export default function DashboardIndexPage() {
     return (
         <React.Fragment>
             <div className='absolute right-0 z-10 flex flex-row items-center w-[80%] xl:w-[86%] h-[10vh] 
                 bg-slate-200 border-b border-slate-300/20 shadow-md'>
-                <Header />
+                <Suspense fallback={<Loader />}>
+                    <Header />
+                </Suspense>
             </div>
 
             <div className='flex-none w-[20%] xl:w-[14%] h-full bg-slate-800'>
-                <Menu />
+                <Suspense fallback={<Loader />}>
+                    <Menu />
+                </Suspense>
             </div>
         </React.Fragment>
     )
-}
+};
