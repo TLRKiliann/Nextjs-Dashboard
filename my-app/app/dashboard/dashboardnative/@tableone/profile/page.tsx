1bdca09d318a731d4deb38f6559a24f17640ca89
TLRKiliann <philogenie@protonmail.com>
2024-06-26 12:55:01 +0200

    aome changes for loading

--- my-app/app/dashboard/dashboardnative/@tableone/profile/page.tsx
+++ my-app/app/dashboard/dashboardnative/@tableone/profile/page.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import TablePage from '@/app/components/TablePage';
 import Profile from '@/app/components/menu-items/Profile';
 import { customers } from '@/app/lib/datadb';
 import ButtonGoBack from '@/app/components/ButtonGoBack';
+import Loader from '@/app/components/Loader';
 
 export default function TrafficPage() {
     return (
@@ -10,11 +11,13 @@ export default function TrafficPage() {
             <div className='h-[10%]'>
                 <h2 className='text-xl'>Profile</h2>
             </div>
-
+            
             <div className='h-[80%]'>
-                <Profile customers={customers} />
+                <Suspense fallback={<Loader />}>
+                    <Profile customers={customers} />
+                </Suspense>
             </div>
-
+            
             <ButtonGoBack text="customers" />
         </TablePage>
     )
