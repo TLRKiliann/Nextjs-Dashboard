f8db0806058f5170eebd37544ca2ffe74c932835
TLRKiliann <philogenie@protonmail.com>
2024-06-27 16:38:16 +0200

    code review

--- my-app/app/components/TableGraphs.tsx
+++ my-app/app/components/TableGraphs.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function TableGraphs({children}: {children: React.ReactNode}) {
     return (
         <div className='xl:w-[85%] xl:h-[85%] bg-slate-900/90 transform duration-300 ease-in-out 
-            hover:scale-105 m-4 xl:m-12 xl:mt-[40px] rounded-lg shadow-out'>
+                hover:scale-105 m-4 xl:m-12 xl:mt-[40px] rounded-lg shadow-out'
+        >
             {children}
         </div>
     )
