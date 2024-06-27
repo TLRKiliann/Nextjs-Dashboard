b40d6f571d77d0b1924d3385f1a9bb3e18263f59
TLRKiliann <philogenie@protonmail.com>
2024-06-27 16:34:04 +0200

    RSC with async function to fetch data from api created

--- my-app/app/products/page.tsx
+++ my-app/app/products/page.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import type { Metadata } from 'next';
+import type { ProductsProps } from '../lib/definitions';
+import React from 'react';
+import AllProducts from '@/app/components/AllProducts';
+//import { products } from '@/app/lib/products';
 
-export default function ProductsPage() {
+export const metadata: Metadata = {
+    title: "Products",
+    description: "access user"
+};
+
+export default async function ProductsPage() {
+    
+    const response = await fetch("http://localhost:3000/api/products");
+    const products = (await response.json()) as ProductsProps[];
+
     return (
-        <div>All Products</div>
+        <>
+            <AllProducts products={products} />
+        </>
     )
-}
+};
