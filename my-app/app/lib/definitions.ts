db76dee0a50d5ee50a17106e8523f1a795ddf770
TLRKiliann <philogenie@protonmail.com>
2024-06-28 13:42:21 +0200

    Geolocation Types moved from api-request.ts

--- my-app/app/lib/definitions.ts
+++ my-app/app/lib/definitions.ts
@@ -1,5 +1,20 @@
 import { StaticImageData } from "next/image";
 
+export type GeoLocationData = {
+    ip:	string;
+    country_code: string;
+    country_name: string;
+    region_name: string;
+    city_name: string;
+    latitude: number;
+    longitude: number;
+    zip_code: string; 
+    time_zone: string; 
+    asn: string; 
+    as: string; 
+    is_proxy: boolean;	
+};
+
 export type CustomersProps = {
     readonly id: number;
     username: string;
