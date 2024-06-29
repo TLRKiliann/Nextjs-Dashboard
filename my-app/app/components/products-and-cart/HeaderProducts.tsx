3895dfc73fe848b76e692ee7983f03ce6821fded
TLRKiliann <philogenie@protonmail.com>
2024-06-29 18:59:04 +0200

    change position to fixed with z-20

--- my-app/app/components/products-and-cart/HeaderProducts.tsx
+++ my-app/app/components/products-and-cart/HeaderProducts.tsx
@@ -5,8 +5,8 @@ import CartItemsQuantity from './CartItemsQuantity';
 
 export default function HeaderProducts() {
     return (
-        <div className='fixed top-0 w-full h-[70px] text-slate-700/90 bg-slate-200 
-            transition duration-300 ease-in-out opacity-100 hover:opacity-100'>
+        <div className='fixed z-20 top-0 w-full h-[70px] text-slate-700/90 bg-slate-200 
+            transition duration-300 ease-in-out opacity-0 hover:opacity-100'>
             <nav className="w-full h-full">
                 <ul className='w-full h-full flex flex-row items-center justify-end'>
 
@@ -19,7 +19,7 @@ export default function HeaderProducts() {
                     </li>
 
                     <li className='mr-6'>
-                        <Link href="/">Contact</Link>
+                        <Link href="/contact">Contact</Link>
                     </li>
 
                     <li className='mr-4'>
@@ -27,10 +27,10 @@ export default function HeaderProducts() {
                     </li>
 
                     <li className='relative flex items-center justify-center w-[40px] h-[40px] 
-                    transform duration-100 ease-in-out hover:scale-105 active:scale-95
+                        transform duration-100 ease-in-out hover:scale-105 active:scale-95
                         hover:text-white hover:bg-blue-200 active:text-slate-200 active:bg-blue-300
                         rounded-full mr-8'>
-                        <Link href="/products/cart" className=''>
+                        <Link href="/products/cart">
                             <FaShoppingCart size={18}/>
                             <CartItemsQuantity />
                         </Link>
