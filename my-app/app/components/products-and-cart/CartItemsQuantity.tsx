c1e2e5ddd06f028c88389a9d84705f870110012a
TLRKiliann <philogenie@protonmail.com>
2024-06-29 18:58:23 +0200

    style improved

--- my-app/app/components/products-and-cart/CartItemsQuantity.tsx
+++ my-app/app/components/products-and-cart/CartItemsQuantity.tsx
@@ -18,8 +18,8 @@ export default function CartItemsQuantity() {
 
     return (
         <div className='absolute top-0'>
-            <p className={`${storeQuantity === 0 ? "opacity-0" : "opacity-100"} flex items-center justify-center border-none w-[24px] h-[24px] text-xs
-                text-slate-200 bg-blue-500 rounded-full mt-[20px] ml-[12px]`}>
+            <p className={`${storeQuantity === 0 ? "opacity-0" : "opacity-100"} flex items-center justify-center 
+                border-none w-[24px] h-[24px] text-xs text-slate-200 bg-blue-500 rounded-full mt-[20px] ml-[12px]`}>
                 {storeQuantity}
             </p>
         </div>
