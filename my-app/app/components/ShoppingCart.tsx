487475d26ae1e0272d91b6f2b4f2dfeef3e46f02
TLRKiliann <philogenie@protonmail.com>
2024-06-29 13:27:02 +0200

    style improved

---
+++ my-app/app/components/ShoppingCart.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import { ProductsProps } from '@/app/lib/definitions';
+import React from 'react';
+import Image from 'next/image';
+import { useStore } from '@/app/lib/store';
+import usePersistStore from '@/app/helpers/usePersistStore';
+
+export default function ShoppingCartPage() {
+
+    // zustand
+    const store = usePersistStore(useStore, (state) => state);
+
+    if (!store) {
+        return <div>Loading...</div>;
+    }
+
+    const handleDeleteProduct = (id: number) => {
+        const findId = store.bearProducts.find((bear) => bear.id === id);
+        if (findId) {
+            store.decreaseQuantity(findId.id);
+        }
+    };
+
+    const handleAddProduct = (id: number) => {
+        const findId = store.bearProducts.find((bear) => bear.id === id);
+        if (findId) {
+            store.increaseQuantity(findId.id);
+        }
+    };
+
+    const handleRemoveAllProducts = (id: number) => {
+        const findId = store.bearProducts.find((bear) => bear.id === id);
+        if (findId) {
+            store.removeAllById(findId.id);
+        }
+    };
+
+    return (
+        <div className='w-full min-h-screen grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 
+            2xl:grid-cols-5 grid-rows-3 text-slate-100 bg-slate-200 gap-4 p-4'>
+        
+            {store?.bearProducts.map((product: ProductsProps) => (
+                <div key={product.id} className="w-[300px] h-[300px] bg-gradient-to-bl from-slate-950 to-slate-700 
+                    m-auto rounded-md shadow-out">
+                    
+                    <div className='w-full h-full flex flex-col items-center justify-between rounded-md'>
+
+                        <div className="w-full h-[70%] flex flex-row items-start justify-center rounded-tl-md rounded-tr-md 
+                            bg-gradient-to-t from-slate-800/70 to-cyan-600">
+                            <div className='w-[140px] h-[80px] border-none rounded-tl-md rounded-br-md shadow-lg'>
+                                <Image src={product.img}
+                                    width={100}
+                                    height={100}
+                                    className="w-full h-full object-fit rounded-tl-md rounded-br-md" 
+                                    alt="no img" 
+                                />
+                            </div>
+                            <div className='flex flex-col items-start justify-center w-full h-full pl-4'>
+                                <h3 className='text-xl font-bold'>{product.family}</h3>
+                                <h4 className='text-lg'>{product.name}</h4>
+                                <h4 className='text-sm my-2'>Version: {product.version}</h4>
+                                
+                                {product.stock === product.quantity ? (
+                                        <p className='text-base text-orange-500'>
+                                            Stock: {product.stock - product.quantity}
+                                        </p>
+                                    ) : (
+                                        <p className='text-base text-cyan-300'>
+                                            Stock: {product.stock - product.quantity}
+                                        </p>
+                                    )
+                                }
+
+                                <p className='text-base font-bold'>Price: {product.price}.-</p>
+                            </div>
+                        </div>
+
+                        <div className='w-full'>
+                            <div className='flex flex-row items-center justify-evenly my-4'>
+                                <button type="button" onClick={() => handleDeleteProduct(product.id)}
+                                    className='w-[40px] h-[40px] hover:text-lg font-bold transform duration-100 ease-in-out hover:scale-105 
+                                        active:scale-95 hover:text-white active:text-slate-300 bg-cyan-600 hover:bg-cyan-400/70 active:bg-cyan-700
+                                        rounded-full disabled:opacity-50 px-[10px] pt-[0px] hover:pt-[1px] active:pt-[0px]'
+                                    disabled={product.quantity < 1 ? true : false}
+                                    aria-label={`Remove one ${product.name}`}
+                                >
+                                    -
+                                </button>
+                                
+                                    <p className='w-[100px] text-center text-sm -mx-10'>Quantity: <span className="text-lg text-cyan-400">{product.quantity}</span></p>
+                                
+                                <button type="button" onClick={() => handleAddProduct(product.id)}
+                                    
+                                    className='w-[40px] h-[40px] hover:text-lg font-bold transform duration-100 ease-in-out hover:scale-105 
+                                        active:scale-95 hover:text-white active:text-slate-300 bg-cyan-600 hover:bg-cyan-400/70 active:bg-cyan-700
+                                        rounded-full disabled:opacity-50 px-[10px] pt-[0px] hover:pt-[1px] active:pt-[0px]'
+                                    disabled={product.stock === product.quantity ? true : false}
+                                    aria-label={`Add one more ${product.name}`}
+                                >
+                                    +
+                                </button>
+                            </div>
+
+                            <div className='flex justify-center mb-4'>
+                                <button type="button" onClick={() => handleRemoveAllProducts(product.id)}
+                                    className='font-bold
+                                        transform duration-100 ease-in-out hover:scale-105 active:scale-95
+                                        bg-red-500 hover:bg-red-600 active:bg-red-700
+                                        px-4 py-1 rounded'
+                                    aria-label={`Remove all ${product.name}`}>Remove</button>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+            ))}
+        </div>
+    )
+}
