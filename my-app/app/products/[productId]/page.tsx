db9a866a6d6112fdaf41ff837db78095c9f3d366
TLRKiliann <philogenie@protonmail.com>
2024-06-30 15:04:03 +0200

    some changes

--- my-app/app/products/[productId]/page.tsx
+++ my-app/app/products/[productId]/page.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
+import { notFound } from 'next/navigation';
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import { getProductsData } from "@/app/utils/api-request";
-//import { notFound } from 'next/navigation';
 import ProductIdCard from '@/app/components/products-and-cart/ProductIdCard';
 
 export default async function ProductIdPage({params}: {params: {productId: string}}) {
 
     if (!parseInt(params.productId)) {
-        //throw new Error("Error: id is not a number");
-        console.log("RSC error: params id...");
+        throw new Error("Error: id is not a number");
     };
 
-    if (parseInt(params.productId) < 1 || parseInt(params.productId) > 100) {
-        //notFound();
-        console.log("RSC error: this product doesn't exist");
+    if (parseInt(params.productId) < 1 || parseInt(params.productId) > 10) {
+        notFound();
     };
 
     // useQuery
