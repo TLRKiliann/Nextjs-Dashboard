4cbbb8c9cd94896b67e11b454b7baa9ffd9cbae2
TLRKiliann <philogenie@protonmail.com>
2024-06-30 18:10:29 +0200

    async function created for login

---
+++ my-app/app/lib/actions.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import { redirect } from "next/navigation";
+
+export type State = {
+    status: "success";
+    message: string;
+} | null | undefined;
+
+export async function onSubmitFunc(prevState: State | null, data: FormData): Promise<State> {
+    try {
+        const response = await fetch('http://localhost:3000/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        if (response.ok) {
+            const responseData = await response.json();
+            console.log(responseData);
+            //redirect('/products');
+            return {
+                status: "success",
+                message: `Welcome, ${data.get("username")} ${data.get("password")}!`,
+            };
+        } else {
+            alert('Invalid credentials');
+            throw new Error('Server validation error');
+        }
+    } catch (error) {
+        console.error((error as Error).message);
+    }
+};
