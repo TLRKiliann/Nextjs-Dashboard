304b7b2f5e7ee5163aded15dd3010b80c1e14418
TLRKiliann <philogenie@protonmail.com>
2024-07-01 14:00:13 +0200

    maxLength for password changed

--- my-app/app/lib/validation.ts
+++ my-app/app/lib/validation.ts
@@ -3,5 +3,5 @@ import { z } from "zod";
 
 export const formSchema = zfd.formData({
     username: zfd.text(z.string().min(4, "Too short").max(20, "Too long")),
-    password: zfd.text(z.string().min(4, "Too short").max(20, "Too long")),
+    password: zfd.text(z.string().min(8, "Too short").max(20, "Too long")),
 });
