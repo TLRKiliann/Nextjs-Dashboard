c4338d3e799d28adc461662c242f1d2e9c6e53bc
TLRKiliann <philogenie@protonmail.com>
2024-07-01 18:05:10 +0200

    formSchemaRegister created

--- my-app/app/lib/validation.ts
+++ my-app/app/lib/validation.ts
@@ -3,5 +3,11 @@ import { z } from "zod";
 
 export const formSchema = zfd.formData({
     username: zfd.text(z.string().min(4, "Too short").max(20, "Too long")),
-    password: zfd.text(z.string().min(8, "Too short").max(20, "Too long")),
+    password: zfd.text(z.string().min(8, "Too short").max(16, "Too long")),
 });
+
+export const formSchemaRegister = zfd.formData({
+    username: zfd.text(z.string().min(4, "Too short").max(20, "Too long")),
+    email: zfd.text(z.string().min(4, "Too short").max(20, "Too long")),
+    password: zfd.text(z.string().min(8, "Too short").max(16, "Too long")),
+})
