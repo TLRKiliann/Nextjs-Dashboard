4ebe1397414f7a1f177d50a62aba2c28d214d87d
TLRKiliann <philogenie@protonmail.com>
2024-07-01 13:37:46 +0200

    server action improved with validation & ZodError

--- my-app/app/lib/actions.ts
+++ my-app/app/lib/actions.ts
@@ -1,11 +1,57 @@
 "use server";
 
-//import { redirect } from "next/navigation";
 import { State } from "./definitions";
+import { formSchema } from "./validation";
+import { ZodError } from "zod";
 
-export async function onSubmitFunc(prevState: State | null, data: FormData): Promise<State> {
+export async function onLoginFunc(prevState: State | null, data: FormData): Promise<State> {
     try {
+        //artificial promise to delete with server
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        // Validate our data
+        const { username, password } = formSchema.parse(data);
         const response = await fetch('http://localhost:3000/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        if (response.ok) {
+            const responseData = (await response.json()) as State ;
+            console.log(responseData);
+            return {
+                status: "success",
+                message: `Welcome, ${username} ${password}!`,
+            };
+        } else {
+            alert('Invalid credentials');
+            throw new Error('Server validation error');
+        }
+    } catch (e) {
+        // In case of a ZodError (caused by our validation) we're adding issues to our response
+        if (e instanceof ZodError) {
+            return {
+                status: "error",
+                message: "Invalid form data",
+                errors: e.issues.map((issue) => ({
+                path: issue.path.join("."),
+                message: `Server validation: ${issue.message}`,
+            })),
+          };
+        }
+        return {
+            status: "error",
+            message: "Something went wrong. Please try again.",
+        };
+    }
+};
+
+export async function onRegisterFunc(prevState: State | null, data: FormData): Promise<State> {
+    try {
+        //artificial promise to delete with server
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        const response = await fetch('http://localhost:3000/api/register', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -15,7 +61,6 @@ export async function onSubmitFunc(prevState: State | null, data: FormData): Pro
         if (response.ok) {
             const responseData = await response.json();
             console.log(responseData);
-            //redirect('/products');
             return {
                 status: "success",
                 message: `Welcome, ${data.get("username")} ${data.get("password")}!`,
