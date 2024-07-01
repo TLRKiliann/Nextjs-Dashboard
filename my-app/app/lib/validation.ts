300c0adf12fbc919e7f3bee4b6933f8c5bad13f7
TLRKiliann <philogenie@protonmail.com>
2024-07-01 13:31:56 +0200

    formSchema with zod setup done

---
+++ my-app/app/lib/validation.ts
@@ -0,0 +1,7 @@
+import { zfd } from "zod-form-data";
+import { z } from "zod";
+
+export const formSchema = zfd.formData({
+    username: zfd.text(z.string().min(4, "Too short").max(20, "Too long")),
+    password: zfd.text(z.string().min(4, "Too short").max(20, "Too long")),
+});
