0de0f917274c4165e47159088da70d1ee0fa19b1
TLRKiliann <philogenie@protonmail.com>
2024-07-03 12:10:54 +0200

    shoppingcart design improved

--- my-app/components/products-and-cart/ShoppingCart.tsx
+++ my-app/components/products-and-cart/ShoppingCart.tsx
@@ -45,9 +45,9 @@ export default function ShoppingCartPage() {
             {storeQuantity > 0 ? (
                 store.bearProducts.map((product: ProductsProps) => (
                 
-                <div key={product.id} className="w-full h-20 flex bg-white rounded-md shadow-sm-out my-1 p-2">
+                <div key={product.id} className="w-full h-20 flex items-center justify-evenly bg-white rounded-md shadow-sm-out my-1 p-2">
                     
-                    <div className='w-[7%] flex items-center justify-center border-none rounded-tl-md 
+                    <div className='w-[70px] flex items-center justify-center border-none rounded-tl-md 
                         rounded-br-md'>
                         <Image src={product.img}
                             width={70}
@@ -58,44 +58,50 @@ export default function ShoppingCartPage() {
                     </div>
 
                     <div className='flex flex-row items-center justify-between w-[60%] h-full px-4'>
-                        <h3 className='w-[70px] text-xl font-bold border border-blue-500'>{product.family}</h3>
-                        <h4 className='w-[50px] text-lg border border-blue-500'>{product.name}</h4>
-                        <h4 className='w-[160px] text-sm border border-blue-500'>Version: {product.version}</h4>
                         
-                        <div className='w-[100px] text-center border border-blue-500'>
+                        <h3 className='sm:w-[50px] lg:w-[70px] sm:text-base lg:text-xl font-bold'>{product.family}</h3>
+
+                        <h4 className='sm:w-[50px] lg:w-[70px] text-center sm:text-base lg:text-xl'>{product.name}</h4>
+                        
+                        <h4 className='w-[160px] text-xs'>Version: {product.version}</h4>
+                        
+                        <div className='w-[100px] h-full flex items-center justify-center bg-slate-100/70 py-10'>
                             {product.stock === product.quantity ? (
-                                    <p className='text-base text-orange-500'>
-                                        Stock: {product.stock - product.quantity}
+                                    <p className='text-sm text-red-500'>
+                                        Stock: <span className='font-bold'>{product.stock - product.quantity}</span>
                                     </p>
                                 ) : (
-                                    <p className='text-base text-blue-500'>
-                                        Stock: {product.stock - product.quantity}
+                                    <p className='text-sm text-blue-500'>
+                                        Stock: <span className='font-bold'>{product.stock - product.quantity}</span>
                                     </p>
                                 )
                             }
                         </div>
 
+                        <div className='flex flex-row items-center justify-around w-[30%]'>
+                            <p className='sm:text-sm lg:text-base font-bold'>
+                                {product.price}.-
+                            </p>
 
-                        <p className='text-base font-bold border border-blue-500'>
-                            Price: {product.price}.-
-                        </p>
+                            <li className='list-none text-xs text-center text-blue-500 hover:text-blue-600 
+                                active:text-blue-700'>
+                                <Link href={`/products/${product.id}`}>View more</Link>
+                            </li>
+                        </div>
 
-                        <li className='list-none text-sm text-center text-blue-500 hover:text-blue-600 
-                            active:text-blue-700 py-4'>
-                            <Link href={`/products/${product.id}`}>View more</Link>
-                        </li>
 
                     </div>
 
-                    <div className='w-[13%] flex items-center justify-center bg-slate-100/70 -my-2'>
-                        <p className='text-center text-base'>
-                            Quantity: <span className="text-lg text-sky-600">{product.quantity}</span>
+                    <div className='w-[13%] flex items-center justify-center bg-slate-100/70 py-8'>
+                        <p className='text-sm text-center text-blue-500'>
+                            Quantity: <span className='font-bold'>{product.quantity}</span>
                         </p>
                     </div>
 
                     <div className='w-[10%] flex flex-row items-center justify-evenly'>
                         <button type="button" onClick={() => handleDeleteProduct(product.id)}
-                            className="w-[38px] h-[38px] text-slate-100 font-bold bg-blue-500 rounded-full shadow-sm-out"
+                            className="w-[38px] h-[38px] text-slate-100 font-bold bg-blue-500 
+                                hover:bg-blue-600/90 active:bg-blue-600 rounded-full shadow-sm-out"
                             disabled={product.quantity < 1 ? true : false}
                             aria-label={`Remove one ${product.name}`}
                         >
@@ -103,7 +109,8 @@ export default function ShoppingCartPage() {
                         </button>
                         
                         <button type="button" onClick={() => handleAddProduct(product.id)}
-                            className="w-[38px] h-[38px] text-slate-100 font-bold bg-blue-500 rounded-full shadow-sm-out"
+                            className="w-[38px] h-[38px] text-slate-100 font-bold bg-blue-500 
+                                hover:bg-blue-600/90 active:bg-blue-600 rounded-full shadow-sm-out"
                             disabled={product.stock === product.quantity ? true : false}
                             aria-label={`Add one more ${product.name}`}
                         >
@@ -113,7 +120,8 @@ export default function ShoppingCartPage() {
 
                     <div className='w-[10%] flex items-center justify-center'>
                         <button type="button" onClick={() => handleRemoveAllProducts(product.id)}
-                            className="text-slate-50 bg-red-500 rounded-full shadow-sm-out px-4 py-1"
+                            className="text-slate-50 bg-red-500 hover:bg-red-600/90 active:bg-red-700
+                                rounded-full shadow-sm-out px-4 py-1"
                             aria-label={`Remove all ${product.name}`}>
                             Remove
                         </button>
