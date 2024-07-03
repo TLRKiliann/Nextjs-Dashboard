625f7d56d54d35547274f7838ee9ee84c9bfc443
TLRKiliann <philogenie@protonmail.com>
2024-07-03 18:38:21 +0200

    permission for User accesss done with nextauth

--- my-app/app/products/layout.tsx
+++ my-app/app/products/layout.tsx
@@ -1,15 +1,15 @@
-//import { auth } from "@/auth";
+import { auth } from "@/auth";
 import React from 'react';
-//import { redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 import HeaderProducts from '@/components/products-and-cart/HeaderProducts';
 
 export default async function ProductLayout({children}: {children: React.ReactNode}) {
     
-    //const session = await auth();
+    const session = await auth();
 
-    /* if (!session?.user) {
+    if (!session?.user) {
         return redirect("/api/auth/signin");
-    } */
+    }
     
     return (
         <div>
