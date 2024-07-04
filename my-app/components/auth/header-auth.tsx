ae35438a8a1d9154f780d0a8942f555db514a3ac
TLRKiliann <philogenie@protonmail.com>
2024-07-04 11:54:46 +0200

    order of titles sorted

--- my-app/components/auth/header-auth.tsx
+++ my-app/components/auth/header-auth.tsx
@@ -1,5 +1,6 @@
 import { auth, signOut } from "@/auth";
 import Link from 'next/link';
+import { FaPowerOff } from "react-icons/fa6";
 
 export const dynamic = "force-dynamic";
 
@@ -15,25 +16,7 @@ const HeaderAuth = async () => {
     const user = session?.user;
 
     return (
-        <div className='fixed top-0 z-10 flex flex-row items-center justify-end w-full h-[60px] bg-sky-200 space-x-10 pr-6'>
-
-            {user && (
-                <form action={logoutAction} className='flex space-x-10'>
-                    <li className="list-none">
-                        <Link href="/profile">Profile</Link>
-                    </li>
-
-                    <li className="list-none">
-                        <Link href="/products">Products</Link>
-                    </li>
-
-                    <p>{user.email}</p>
-
-                    <li className="list-none">
-                        <button>Logout</button>
-                    </li>
-                </form>
-            )}
+        <div className='fixed top-0 z-10 flex flex-row items-center justify-end w-full h-[60px] bg-sky-200 pr-10'>
 
             {!user && (
                 <>
@@ -49,6 +32,24 @@ const HeaderAuth = async () => {
             <li className="list-none">
                 <Link href="/dashboard/dashboardnative">Dashboard (admin)</Link>
             </li>
+
+            {user && (
+                <form action={logoutAction} className='flex space-x-8'>
+                    <li className="list-none">
+                        <Link href="/profile">Profile</Link>
+                    </li>
+
+                    <li className="list-none">
+                        <Link href="/products">Products</Link>
+                    </li>
+
+                    <p className="text-blue-500">{user.name}</p>
+
+                    <li className="relative flex list-none">
+                        <button><FaPowerOff size={16} /></button>
+                    </li>
+                </form>
+            )}
 
         </div>
     )
