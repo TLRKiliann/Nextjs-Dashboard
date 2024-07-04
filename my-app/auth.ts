a851e8fe124db6b3718c16f66577cf986e556610
TLRKiliann <philogenie@protonmail.com>
2024-07-04 10:07:01 +0200

    permission path updated

--- my-app/auth.ts
+++ my-app/auth.ts
@@ -49,7 +49,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const paths = ["/profile", "/products"];
+      const paths = ["profile", "/products", "/dashboard/dashboardnative"];
       const isProtected = paths.some((path) =>
         nextUrl.pathname.startsWith(path)
       );
