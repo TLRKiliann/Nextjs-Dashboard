356917d8b3d19d8c09b59a4fbc62b4c1167fecb4
TLRKiliann <philogenie@protonmail.com>
2024-07-05 18:23:43 +0200

    order route created with all what we need for payment

---
+++ my-app/app/order/payment/[orderId]/layout.tsx
@@ -0,0 +1,21 @@
+import { Metadata } from 'next';
+import React from 'react';
+
+export const generateMetadata = async ({params}: {params: {orderId: string}}): Promise<Metadata> => {
+    const title = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`method ${params.orderId}`)
+        }, 300)
+    })
+    return {
+        title: `Order ${title}`
+    }
+}
+
+export default function OrderIdLayout({children}: {children: React.ReactNode}) {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
