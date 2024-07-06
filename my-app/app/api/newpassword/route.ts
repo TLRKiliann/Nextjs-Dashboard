af81837388c4375a972352dcdea65cacce0d6499
TLRKiliann <philogenie@protonmail.com>
2024-07-06 17:04:30 +0200

    more adapted route

--- my-app/app/api/newpassword/route.ts
+++ my-app/app/api/newpassword/route.ts
@@ -1,4 +1,4 @@
-import { hash } from "bcryptjs";
+import { hash, compare } from "bcryptjs";
 import { NextResponse } from "next/server";
 import prisma from "@/prisma/prisma";
 import { newPasswordSchema } from "@/lib/user-schema";
@@ -6,35 +6,52 @@ import { ZodError } from "zod";
 
 export async function POST(req: Request): Promise<NextResponse> {
     try {
-        const { password, userId } = newPasswordSchema.parse(await req.json());
+        const { email, password, newPassword } = newPasswordSchema.parse(await req.json());
+
         const user = await prisma.user.findUnique({
-            where: { id: userId },
+            where: { email: email.toLowerCase() },
         });
 
-        if (!user) {
+        if (!user || !user.password) {
             return NextResponse.json(
                 {
                     status: "fail",
-                    message: "User not found",
+                    message: "User not found or user has no password set",
                 },
                 { status: 404 }
             );
         }
-        const hashedPassword = await hash(password, 10);
+
+        const isPasswordValid = await compare(password, user.password as string);
+        if (!isPasswordValid) {
+            return NextResponse.json(
+                {
+                    status: "fail",
+                    message: "Current password is incorrect",
+                },
+                { status: 401 } // HTTP 401 Unauthorized
+            );
+        }
+
+        const hashedNewPassword = await hash(newPassword, 12);
+
+        const userEmail = user.email as string;
 
         await prisma.user.update({
-            where: { id: userId },
-            data: { password: hashedPassword },
-        });
-
-        return NextResponse.json({
-            status: "success",
-            message: "Password updated successfully",
-            user: {
-                id: user.id,
-                email: user.email,
+            where: { email: userEmail },
+            data: {
+                password: hashedNewPassword,
             },
         });
+
+        return NextResponse.json(
+            {
+                status: "success",
+                message: "Password updated successfully",
+            },
+            { status: 200 }
+        );
+
     } catch (error: any) {
         if (error instanceof ZodError) {
             return NextResponse.json(
