58806dc70138a8f637709bb8d670c648bb10a1f5
TLRKiliann <philogenie@protonmail.com>
2024-07-06 15:17:11 +0200

    signin improved with a creation of cookie with token

--- my-app/app/api/signin/route.ts
+++ my-app/app/api/signin/route.ts
@@ -3,6 +3,11 @@ import { NextResponse } from "next/server";
 import prisma from "@/prisma/prisma";
 import { loginUserSchema } from "@/lib/user-schema";
 import { ZodError } from "zod";
+import { cookies } from "next/headers";
+import jwt from 'jsonwebtoken';
+import { serialize } from 'v8';
+
+const SECRET_KEY = process.env.SECRET_KEY || 'your-secret-key';
 
 export async function POST(req: Request): Promise<NextResponse> {
   try {
@@ -11,7 +16,6 @@ export async function POST(req: Request): Promise<NextResponse> {
     const user = await prisma.user.findUnique({
       where: { email: email.toLowerCase() },
     });
-
     if (!user) {
       return NextResponse.json(
         {
@@ -21,7 +25,6 @@ export async function POST(req: Request): Promise<NextResponse> {
         { status: 401 }
       );
     }
-
     if (!user.password) {
         return NextResponse.json(
           {
@@ -31,10 +34,8 @@ export async function POST(req: Request): Promise<NextResponse> {
           { status: 500 }
         );
     }
-
     const isPasswordValid = await compare(password, user.password);
-
-
+    
     if (!isPasswordValid) {
       return NextResponse.json(
         {
@@ -44,15 +45,31 @@ export async function POST(req: Request): Promise<NextResponse> {
         { status: 401 }
       );
     }
+    if (user.email && user.password) {
+      // set TOKEN
+      const cookie = cookies();
+      const token = jwt.sign({ email: user.email }, SECRET_KEY, { expiresIn: '1h' });
 
-    return NextResponse.json({
-      status: "success",
-      message: "Logged in successfully",
-      user: {
-        name: user.name,
-        email: user.email,
-      },
-    });
+      const cookieOptions = {
+          httpOnly: true,
+          path: '/',
+          secure: process.env.NODE_ENV === 'production',
+          sameSite:'strict'
+      }; 
+      // set COOKIE with TOKEN
+      cookie.set('Set-Cookie', `auth-token=${token}; ${serialize(cookieOptions)}`, {sameSite:'strict'});
+      return NextResponse.json({message: "response from POST", 
+        user: {
+          name: user.name,
+          email: user.email,
+        }}, 
+        {status: 200, headers: {
+          "Content-Type": "application/json"
+      }});
+
+    } else {
+      return NextResponse.json("Login failed");
+    }
   } catch (error: any) {
     if (error instanceof ZodError) {
       return NextResponse.json(
@@ -64,7 +81,6 @@ export async function POST(req: Request): Promise<NextResponse> {
         { status: 400 }
       );
     }
-
     return NextResponse.json(
       {
         status: "error",
