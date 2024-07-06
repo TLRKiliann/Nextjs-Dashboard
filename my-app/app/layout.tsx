7bea4d9725e5c148535f1350405d32fd431a88c3
TLRKiliann <philogenie@protonmail.com>
2024-07-06 15:23:58 +0200

    SessionProvider auth added

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,31 +1,31 @@
-//import { useState } from "react";
-import { auth } from '@/auth';
+"use client";
+
+import { useState } from "react";
 import { SessionProvider } from 'next-auth/react'
 import { Toaster } from "react-hot-toast";
-//import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 //import { Inter } from "next/font/google";
-//import Header from "@/components/Header";
 import "./globals.css";
 
 //const inter = Inter({ subsets: ["latin"] });
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await auth();
-  //const [queryClient] = useState(() => new QueryClient());
+
+  const [queryClient] = useState(() => new QueryClient());
   return (
-    <SessionProvider session={session}>
+    <SessionProvider>
       <html lang="en">
         <body>
-          {/* <QueryClientProvider client={queryClient}> */}
+          <QueryClientProvider client={queryClient}>
             <Toaster position="bottom-center" />
             <main>
               {children}
             </main>
-          {/* </QueryClientProvider> */} 
+          </QueryClientProvider> 
         </body>
       </html>
     </SessionProvider>
