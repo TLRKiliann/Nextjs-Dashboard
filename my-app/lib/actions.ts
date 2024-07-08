5f709d009d089c2478d453e2441fc371e272d5d5
TLRKiliann <philogenie@protonmail.com>
2024-07-08 20:22:50 +0200

    actions to increment & decrement quantity by id

--- my-app/lib/actions.ts
+++ my-app/lib/actions.ts
@@ -1,11 +1,47 @@
 "use server";
 
-import { ProductsProps } from "./definitions";
+import prisma from "@/prisma/prisma";
+import { revalidatePath } from "next/cache";
+//import { auth } from "@/auth";
 
-export const getProductsData = async () => {
-    const response = await fetch("http://localhost:3000/api/products", {
-        cache: "no-cache"
-    });
-    const products = (await response.json()) as ProductsProps[];
-    return products;
+//update
+export const addProductToDb = async (formData: FormData) => {
+    try {
+        await prisma.post.update({
+            data: {
+                id: Number(formData.get("id")),
+                quantity: {
+                    increment: 1,
+                },
+            },
+            where: {
+                id: Number(formData.get("id")),
+            },
+        });
+        revalidatePath("/testcart");
+    } catch (error) {
+        console.log("Error: ", error)
+        throw new Error("Error: prisma.post.create");
+    }
+};
+
+
+export const deleteProductToDb = async (formData: FormData) => {
+    try {
+        await prisma.post.update({
+            data: {
+                id: Number(formData.get("id")),
+                quantity: {
+                    increment: -1,
+                },
+            },
+            where: {
+                id: Number(formData.get("id")),
+            },
+        });
+        revalidatePath("/testcart");
+    } catch (error) {
+        console.log("Error: ", error)
+        throw new Error("Error: prisma.post.create");
+    }
 };
