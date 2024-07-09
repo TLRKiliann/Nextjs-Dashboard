e9597df9dfd634a2eb42884582ba7c1d84bc6930
TLRKiliann <philogenie@protonmail.com>
2024-07-09 19:27:21 +0200

    products fetched from db postgresql

--- my-app/components/AllProducts.tsx
+++ my-app/components/AllProducts.tsx
@@ -1,43 +1,37 @@
-"use client";
+import prisma from '@/prisma/prisma';
+import { Suspense } from 'react';
+import Card from '@/components/products-and-cart/Card';
+import Loader from '@/components/Loader';
 
-import type { ProductsProps } from '../lib/definitions';
-import React, { useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
-import { getProductsData } from '@/lib/actions';
-import Card from './products-and-cart/Card';
-import Loader from './Loader';
-
-export default function AllProducts() {
-   
-    // useQuery
-    const { data, isLoading, isError, error } = useQuery<ProductsProps[]>({
+export default async function AllProducts() {
+    
+    // useQuery is not recommanded with server action
+    /* const { data, isLoading, isError, error } = useQuery<ProductsProps[]>({
         queryKey: ["products"],
         queryFn: () => getProductsData(),
         staleTime: 10 * 1000,
+    }); */
+
+    const products = await prisma.product.findMany({
+        orderBy: {
+            id: "asc",
+        }
     });
 
-    const [database] = useState<ProductsProps[]>(data!);
-
-    if (isLoading) {
-        return <Loader />
-    };
-
-    if (!data) {
-        throw new Error("Error: problem with data (useQuery)");
-    };
-
-    if (isError) {
-        throw new Error("Error - useQuery: ", error);
+    if (!products) {
+        throw new Error("Error: useQuery + server action");
     };
 
     return (
         <div className='min-h-screen grid grid-cols-3 xl:grid-cols-4 grid-rows-3 bg-slate-100 gap-4 p-4 pt-24'>
-            {database?.map((product: ProductsProps) => (
-                <Card 
-                    key={product.id}
-                    product={product}
-                />                
-            ))}
+            <Suspense fallback={<Loader />}>
+                {products?.map((product) => (
+                    <Card
+                        key={product.id}
+                        product={product}
+                    /> 
+                ))}
+            </Suspense>
         </div>
     )
 };
