e19743ec03a8820a421d5036a5c0dc2f2aad3c21
TLRKiliann <philogenie@protonmail.com>
2024-07-10 12:44:40 +0200

    HeaderAuth added with export dynamic

--- my-app/app/(auth)/layout.tsx
+++ my-app/app/(auth)/layout.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from 'next';
 import React from 'react';
-//import HeaderAuth from '@/components/auth/header-auth';
+import HeaderAuth from '@/components/auth/header-auth';
+
+export const dynamic = "force-dynamic";
 
 export const metadata: Metadata = {
     title: {
@@ -15,7 +17,7 @@ export default function AuthLayout({children}: {children: React.ReactNode}) {
         <div className="w-full min-h-screen flex items-center justify-center text-base text-slate-800/70 
             bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-white from-0% to-sky-500 
             to-70%">
-            {/* <HeaderAuth /> */}
+            <HeaderAuth />
             {children}
         </div>
     )
