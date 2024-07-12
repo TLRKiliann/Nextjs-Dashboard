9853e2ae6dd420436c9c3360f92e6dfa2f48d7dd
TLRKiliann <philogenie@protonmail.com>
2024-07-12 18:34:47 +0200

    some changes

--- my-app/middleware.ts
+++ my-app/middleware.ts
@@ -1,8 +1,8 @@
 import { auth } from './auth';
-export { auth as middleware } from './auth';
+//export { auth as middleware } from './auth';
 
 export default auth((req) => {
-  if (!req.auth && req.nextUrl.pathname !== "/") {
+  if (!req.auth && req.nextUrl.pathname !== "/login") {
     const newUrl = new URL("/login", req.nextUrl.origin)
     return Response.redirect(newUrl)
   }
