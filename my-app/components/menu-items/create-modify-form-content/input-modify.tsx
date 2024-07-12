bd21fcf3d0f5613facbed456f8d9457914908929
TLRKiliann <philogenie@protonmail.com>
2024-07-12 15:46:30 +0200

    types review & required added & some changes

--- my-app/components/menu-items/create-modify-form-content/input-modify.tsx
+++ my-app/components/menu-items/create-modify-form-content/input-modify.tsx
@@ -10,8 +10,9 @@ type InputModifyProps = {
 export default function InputModify({type, value, onChange, placeholder}: InputModifyProps) {
     return (
         <input type={type} value={value} onChange={onChange} 
-            placeholder={placeholder} 
-            className='w-1/5 bg-slate-50 border border-slate-500/70 outline-none ring-none  
+            placeholder={placeholder}
+            required
+            className='w-[15%] bg-slate-50 border border-slate-500/70 outline-none ring-none  
                 focus:border focus:outline focus:ring focus:border-blue-400 focus:outline-blue-200 
                 focus:ring-blue-300 focus:bg-white rounded ml-2 px-2 py-1'
         />
