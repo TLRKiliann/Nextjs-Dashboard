5b3b65c103de10cec330dccdd0ba68b1aaca0cb3
TLRKiliann <philogenie@protonmail.com>
2024-07-13 17:32:43 +0200

    some changes

--- my-app/components/contact/email-form.tsx
+++ my-app/components/contact/email-form.tsx
@@ -9,7 +9,6 @@ export default function EmailForm() {
     const [textEmail, setTextEmail] = useState<string>("");
     const [textArea, setTextArea] = useState<string>("");
 
-
     const handleTextEmail = (event: React.ChangeEvent<HTMLInputElement>): void => {
         setTextEmail(event.target.value);
     };
@@ -31,8 +30,7 @@ export default function EmailForm() {
 
     return (
         <form action={onSubmit}>
-            <div>
-                <label htmlFor=""></label>
+            <div className='mb-8'>
                 <input
                     type="email"
                     id="email"
@@ -40,28 +38,34 @@ export default function EmailForm() {
                     value={textEmail}
                     onChange={(e) => handleTextEmail(e)}
                     placeholder='email@example.com'
+                    className='form-control block w-full px-4 py-3 text-sm font-normal text-gray-700 
+                        bg-white bg-clip-padding border border-solid border-gray-300 rounded transition 
+                        ease-in-out focus:text-gray-700 focus:bg-white focus:border-blue-600 
+                        focus:outline-none'
                     required
                 />
             </div>
 
-            <div className='my-4'>
-                <textarea name="message" id="message" cols={26} rows={10}
+            <div className='my-8'>
+                <textarea name="message" id="message" cols={26} rows={8}
                     value={textArea}
                     onChange={(e) => handleTextArea(e)}
                     placeholder="Enter your message here..."
-                    className='text-lg text-slate-600 px-2 py-1 bg-white
-                        focus:bg-slate-50/10 placeholder:text-base placeholder:text-slate-400 rounded'>
+                    className='text-base font-normal text-gray-700  px-2 py-2 bg-white
+                        border border-gray-300 focus:border-blue-600 focus:outline-none
+                        placeholder:text-sm placeholder:text-slate-400 rounded'>
                 </textarea>
             </div>
 
-            <div className='mb-4'>
+            <div className='w-full mb-8'>
                 <button
                     type="submit"
-                    className='text-base font-bold bg-blue-500 text-slate-50 px-6 py-2 rounded'
+                    className='w-full text-base font-bold text-slate-50 bg-blue-500 hover:bg-blue-600 
+                        active:bg-blue-700 px-6 py-2 rounded shadow-lg active:shadow-none'
                 >
                     Submit
                 </button>
             </div>
         </form>
     )
-}
+};
