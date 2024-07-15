223e6d6971ca844593c00a975dfae87ab07ddcc4
TLRKiliann <philogenie@protonmail.com>
2024-07-15 10:58:31 +0200

    scroll-overflow created for 2 parts of the page

--- my-app/components/header-items/EmailsAdmin.tsx
+++ my-app/components/header-items/EmailsAdmin.tsx
@@ -21,61 +21,60 @@ export default async function EmailsAdmin() {
         }
     });
 
-    // create scroll to display severals msg
-
     return (
-        <div className='flex flex-row items-start w-full h-[90%] text-slate-900 mt-[10vh]'>
+        <div className='flex flex-row items-start w-full h-[90%] text-slate-800 mt-[10vh]'>
 
-            <div className='w-[500px]'>
+            <div className='relative mt-0 z-10 w-[35%] h-[100%]'>
 
-                {emailBox.map((email: EmailProps) => (
-                    <div key={email.id} className='w-full h-full bg-white border-b border-slate-300 p-2'>
+                <div className='absolute -z-10 w-full h-full overflow-y-scroll no-scrollbar'>
 
-                        <div className='flex justify-end w-full bg-slate-900'>
-                            {email.isOpen === false ? (
-                                <OpenEmail 
-                                    id={email.id}
+                    {emailBox.map((email: EmailProps) => (
+                        <div key={email.id} className='w-full h-auto bg-white border-b border-slate-300 p-2'>
+
+                            <div className='flex justify-end w-full bg-slate-900'>
+                                {email.isOpen === false ? (
+                                    <OpenEmail 
+                                        id={email.id}
+                                    />
+                                ) : (
+                                    <CloseEmail 
+                                        id={email.id}
+                                    />
+                                )}
+
+                                <RemoveEmail 
+                                    id={email.id} 
                                 />
-                            ) : (
-                                <CloseEmail 
-                                    id={email.id}
-                                />
-                            )}
 
-                            <RemoveEmail 
-                                id={email.id} 
-                            />
-
+                            </div>
+                            
+                            <p className='text-base font-bold pt-2'>{email.email}</p>
+                            <p className='text-sm py-2'>{String(email.createdAt).slice(0, 24)}</p>
                         </div>
-                        
-                        <p className='text-base font-bold pt-2'>{email.email}</p>
-                        <p className='text-sm py-2'>{String(email.createdAt).slice(0, 24)}</p>
-                    </div>
-                ))}
-
+                    ))}
+                </div>
             </div>
 
-            <div className='w-full border border-red-500'>
+            <div className='relative mt-0 z-10 w-[65%] h-[100%]'>
 
-                {emailBox.map((email: EmailProps) => email.isOpen === true ? (
+                <div className='absolute -z-10 w-full h-full overflow-y-scroll no-scrollbar'>
 
-                    <div key={email.id} className='w-full h-full bg-white'>
-
-                        <div className='w-full h-auto text-slate-50 bg-slate-800 p-2'>
-                            <p className='text-base font-bold py-1'>{email.email}</p>
-                            <p className='text-sm pb-1'>{String(email.createdAt).slice(0, 24)}</p>
+                    {emailBox.map((email: EmailProps) => email.isOpen === true ? (
+                        <div key={email.id} className='w-full h-auto bg-white'>
+                            <div className='w-full h-auto text-slate-50 bg-slate-800 rounded-tl rounded-bl p-2'>
+                                <p className='text-base font-bold py-1'>{email.email}</p>
+                                <p className='text-sm pb-1'>{String(email.createdAt).slice(0, 24)}</p>
+                            </div>
+                            <div className='text-justify w-full h-auto bg-white border-l border-slate-200 p-4'>
+                                <div className='flex flex-col items-start justify-between'>
+                                    <p className='text-lg'>{email.message}</p>
+                                    <p className='text-base font-bold text-slate-700 mt-10'>{email.email}</p>
+                                </div>
+                            </div>
                         </div>
-
-                        <div className='flex flex-col items-start justify-between w-full h-auto bg-white p-2'>
-                            <p className='text-lg mb-10'>{email.message}</p>
-                            <p className='text-base font-bold text-slate-700 py-1'>{email.email}</p>
-                        </div>
-
-                    </div>
-
-                ) : null)}
-
+                    ) : null)}
+                </div>
             </div>
         </div>
     )
-}
+};
