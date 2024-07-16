0f7f756e1d1e6d4e17e2d70d8daaf3f68325521d
TLRKiliann <philogenie@protonmail.com>
2024-07-16 10:37:21 +0200

    reusable tailwind added

--- my-app/components/TableGraphs.tsx
+++ my-app/components/TableGraphs.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function TableGraphs({children}: {children: React.ReactNode}) {
     return (
-        <div className='xl:w-[85%] xl:h-[85%] bg-slate-900/90 transform duration-300 ease-in-out 
-                hover:scale-105 m-4 xl:m-12 xl:mt-[40px] rounded-lg shadow-out'
-        >
-            {children}
+        <div className='w-full h-full bg-slate-800 border border-slate-700/30'>
+            <div className='flex items-center justify-center w-full h-full'>
+                {children}
+            </div>
         </div>
     )
 }
