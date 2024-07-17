dc0f62452188933d8e8ff1710458dcc5d5787508
TLRKiliann <philogenie@protonmail.com>
2024-07-17 08:16:16 +0200

    user is deconnected when logout

--- my-app/components/auth/header-auth.tsx
+++ my-app/components/auth/header-auth.tsx
@@ -1,8 +1,9 @@
 import { auth, signOut } from "@/auth";
 import Link from 'next/link';
-import { FaPowerOff } from "react-icons/fa6";
-import AdminAccessLink from "./admin-access-link";
+import prisma from "@/prisma/prisma";
 import Image from 'next/image';
+import AdminAccessLink from "./admin-access-link";
+import { FaPowerOff } from "react-icons/fa6";
 import dashLogo from '@/public/assets/images/logo/dash-logo.png';
 
 const HeaderAuth = async () => {
@@ -12,6 +13,15 @@ const HeaderAuth = async () => {
 
     const logoutAction = async () => {
         'use server';
+        await prisma.user.update({
+            data: {
+                email: user?.email!,
+                isConnected: false,
+            },
+            where: {
+                email: user?.email!,
+            }
+        });
         await signOut({
             redirect: true,
             redirectTo: 'http://localhost:3000/login',
