ec3f3d6c38e9bdcd98ce9a8fd7414953e406f6b1
TLRKiliann <philogenie@protonmail.com>
2024-07-18 19:40:48 +0200

    write to db with prisma added with revalidatePath(/profile)

--- my-app/app/api/imgupload/route.ts
+++ my-app/app/api/imgupload/route.ts
@@ -1,8 +1,11 @@
+import { auth } from "@/auth";
+import prisma from "@/prisma/prisma";
 import { readdir, writeFile } from "fs/promises";
+import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 import path from 'path';
 
-export const GET = async (request: Request): Promise<NextResponse | string> => {
+export const GET = async (request: Request): Promise<NextResponse> => {
     const files = await readdir("./public/assets/images/upload");
     return NextResponse.json({msg: "image get successfully", files});
 }
@@ -19,8 +22,23 @@ export const POST = async (request: Request): Promise<NextResponse> => {
         const pathOfImage = `./public/assets/images/upload/${new Date().getTime()}${path.extname(image.name)}`;
         await writeFile(pathOfImage, bufferData);
 
-        console.log(pathOfImage);
-    };
+        const session = await auth();
+        const user = session?.user;
+        if (!user) {
+            throw new Error("An error occured during definition of auth");
+        };
 
+        const pathToDb = pathOfImage.slice(8);
+        await prisma.user.update({
+            data: {
+                email: user.email!,
+                image: pathToDb,
+            },
+            where: {
+                email: user.email!,
+            }
+        })
+        revalidatePath("/profile");
+    };
     return NextResponse.json({msg: "image upload successfully"});
 };
