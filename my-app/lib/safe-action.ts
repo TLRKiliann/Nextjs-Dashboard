a254870e456e2716091d2b861240b4bc75b6e86c
TLRKiliann <philogenie@protonmail.com>
2024-07-20 23:02:49 +0200

    under development

---
+++ my-app/lib/safe-action.ts
@@ -0,0 +1,68 @@
+//import { auth } from '@/auth';
+import { createSafeActionClient, DEFAULT_SERVER_ERROR_MESSAGE  } from 'next-safe-action';
+import { z } from 'zod';
+
+export class ActionError extends Error{};
+
+export const actionClient = createSafeActionClient({
+  defaultValidationErrorsShape: "flattened",
+  defineMetadataSchema() {
+    return z.object({
+      id: z.number(),
+    });
+  },
+  handleReturnedServerError(e) {
+    if (e instanceof ActionError) {
+      return {
+        serverError: e.message,
+      }
+    }
+    return DEFAULT_SERVER_ERROR_MESSAGE 
+    //return "Oh no, something went wrong!";
+
+    /* return {
+      serverError: "Something went wrong!"
+    } */
+  },
+});
+
+
+/* export const actionClient = createSafeActionClient({
+  //defaultValidationErrorsShape: "flattened",
+  handleReturnedServerError(e) {
+    if (e instanceof ActionError) {
+      return {
+        serverError: e.message,
+      }
+    }
+    return DEFAULT_SERVER_ERROR_MESSAGE 
+    //return "Oh no, something went wrong!";
+
+    return {
+      serverError: "Something went wrong!"
+    }
+  },
+}); */
+
+
+export const AuthenticatedAction = createSafeActionClient({
+  handleReturnedServerError(e) {
+    if (e instanceof ActionError) {
+      return {
+        serverError: e.message,
+      }
+    }
+    return {
+      serverError: "Something went wrong!"
+    }
+  },
+  /* async middleware() {
+    const session = await auth();
+    if (!session?.user.email) {
+      throw new Error("Invalid session");
+    }
+    return {
+      userId: session.user.email
+    }
+  } */
+});
