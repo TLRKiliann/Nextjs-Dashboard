bd0ff9488221fe8f1c0fd4ca3c96a0a204afeb11
TLRKiliann <philogenie@protonmail.com>
2024-07-21 14:11:59 +0200

    code review to solve {browser: null} in utils/browseros-data.json

--- my-app/app/api/profile/browseros/route.ts
+++ my-app/app/api/profile/browseros/route.ts
@@ -1,16 +1,20 @@
 import { readFile, writeFile } from "fs/promises";
-import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
     const body = await request.json();
+    if (body === null) {
+        return NextResponse.json({message: "browser failed"});
+    };
     const browser = body;
     const filename = './utils/browseros-data.json';
     try {
-        const nextBrowser = {browser};
+        const nextBrowser = { browser };
+    
         const file = await readFile(filename, { encoding: 'utf8' });
         const prevBrowsers = JSON.parse(file);
         prevBrowsers.push(nextBrowser);
+
         await writeFile(filename, JSON.stringify(prevBrowsers, null, 4));
         console.log('Data has been written to browseros-data.json');
     } catch (error) {
