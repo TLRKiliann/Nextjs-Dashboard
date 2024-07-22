c26410da2a3d7f36fa5d0b20b53c29c6bd91fc60
TLRKiliann <philogenie@protonmail.com>
2024-07-22 11:45:39 +0200

    retrieve public ip added

--- my-app/app/api/profile/browseros/route.ts
+++ my-app/app/api/profile/browseros/route.ts
@@ -1,3 +1,4 @@
+import { ApiPublicIp } from "@/utils/api-request";
 import { readFile, writeFile } from "fs/promises";
 import { NextResponse } from "next/server";
 
@@ -20,5 +21,30 @@ export async function POST(request: Request) {
     } catch (error) {
         throw new Error('An error occurred while writing to browseros-data.json:');
     };
+
+    try {
+        const ipResult = await ApiPublicIp();
+
+        if (!ipResult) {
+            throw new Error("No ip public detected");
+        } else {
+            console.log("Public IP detected");
+        };
+    
+        const filename = './utils/ip-data.json';
+        try {
+            const file = await readFile(filename, { encoding: 'utf8' });
+            const previousIp = JSON.parse(file);
+            previousIp.push(ipResult);
+
+            await writeFile(filename, JSON.stringify(previousIp, null, 4));
+            console.log('Data has been written to ip-data.json');
+        } catch (err) {
+            throw new Error('An error occurred while writing to ip-data.json:');
+        };
+
+    } catch (error) {
+        throw new Error('An error occurred while writing to browseros-data.json:'); 
+    }
     return NextResponse.json({message: "browser & os detected"});
 }
