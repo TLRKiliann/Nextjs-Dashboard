1d730cd9b468d9d553c9ff03b21491c4c3eb5742
TLRKiliann <philogenie@protonmail.com>
2024-07-22 18:00:23 +0200

    fetch data with prisma not by api

--- my-app/app/dashboard/dashboardnative/@tableone/default.tsx
+++ my-app/app/dashboard/dashboardnative/@tableone/default.tsx
@@ -1,15 +1,29 @@
-import type { CustomersProps } from '@/lib/definitions';
+//import type { CustomersProps } from '@/lib/definitions';
+import { PrismaClient, type User } from '@prisma/client';
 import Image from 'next/image';
 import TablePage from '@/components/TablePage';
 
+const prisma = new PrismaClient();
+
 export default async function TableOneDefault() {
 
-    const response = await fetch("http://localhost:3000/api/customers");
+    /* const response = await fetch("http://localhost:3000/api/customers");
     if (!response) {
         //throw new Error("Error: server cannot fetch customers");
         console.error("Error with server, unable to obtain response for clients");
     };
+    
     const customers = (await response.json()) as CustomersProps[];
+ */
+    const users: User[] = await prisma.user.findMany({
+        orderBy: {
+            id: "asc"
+        }
+    })
+
+    if (!users) {
+        throw new Error("users cannot be fetched by prisma");
+    }
 
     return (
         <TablePage title="Members" url="" link="">
@@ -19,18 +33,18 @@ export default async function TableOneDefault() {
                 <ul className='w-full h-[100%] bg-slate-100 overflow-y-scroll no-scrollbar
                     flex flex-col items-center rounded-lg px-2 shadow-in'>
 
-                    {customers.map((customer: CustomersProps) => (
+                    {users.map((customer: User) => (
                         <li key={customer.id} className='w-full bg-slate-50 my-2 shadow-sm-out rounded-lg'>
 
                             <div className='flex items-center justify-between text-slate-500/90 px-2'>
                                 
                                 <div className='flex flex-row items-center justify-start'>
-                                    <Image priority src={customer.img} width={50} height={50} alt="no-img" 
-                                        className='w-[50px] h-[50px] border border-slate-500 object-cover my-1 rounded-full'/>
+                                    <Image priority src={String(customer.image)} width={500} height={333} alt="no-img" 
+                                        className='w-[50px] h-auto border border-slate-500 object-cover my-1 rounded-full'/>
 
                                     <div className='w-[80px] mx-2'>
-                                        <p className='text-base font-bold'>{customer.username}</p>
-                                        <p className='text-xs'>{customer.country}</p>
+                                        <p className='text-base font-bold'>{customer.name}</p>
+                                        {/* <p className='text-xs'>{customer.country}</p> */}
                                     </div>
                                 </div>
 
@@ -48,7 +62,7 @@ export default async function TableOneDefault() {
                                         )}
                                     </div>
                                 
-                                    <p className='flex items-center justify-end text-sm font-bold'>{customer.spend}.-</p>    
+                                    {/* <p className='flex items-center justify-end text-sm font-bold'>{customer.spend}.-</p> */}    
                                 </div>
 
                             </div>
@@ -56,6 +70,7 @@ export default async function TableOneDefault() {
                     ))}
                 </ul>
             </div>
+
         </TablePage>
     )
 }
