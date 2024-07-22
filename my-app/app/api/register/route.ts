8c7d36fc15891fc3df20351f36649272adf438fd
TLRKiliann <philogenie@protonmail.com>
2024-07-22 18:00:53 +0200

    file updated

--- my-app/app/api/register/route.ts
+++ my-app/app/api/register/route.ts
@@ -1,6 +1,6 @@
 import { hash } from "bcryptjs";
 import { NextResponse } from "next/server";
-import prisma from "@/prisma/prisma";
+import { PrismaClient } from "@prisma/client";
 import { createUserSchema } from "@/lib/user-schema";
 import { ZodError } from "zod";
 import { cookies } from "next/headers";
@@ -9,12 +9,14 @@ import { serialize } from 'v8';
 
 const SECRET_KEY = process.env.SECRET_KEY || 'your-secret-key';
 
+const prisma = new PrismaClient();
+
 export async function POST(req: Request): Promise<NextResponse> {
   try {
     const { name, email, password } = createUserSchema.parse(await req.json());
 
     const hashedPassword = await hash(password, 12);
-
+    
     const existingUser = await prisma.user.findUnique({
       where: { email: email.toLowerCase() },
     });
@@ -27,7 +29,6 @@ export async function POST(req: Request): Promise<NextResponse> {
         { status: 409 }
       );
     }
-
     const newUser = await prisma.user.create({
       data: {
         name,
@@ -82,61 +83,4 @@ export async function POST(req: Request): Promise<NextResponse> {
       { status: 500 }
     );
   }
-}
-
-/*
-import { hash } from "bcryptjs";
-import { NextResponse } from "next/server";
-import prisma from "@/prisma/prisma";
-import { createUserSchema } from "@/lib/user-schema";
-import { ZodError } from "zod";
-
-export async function POST(req: Request): Promise<NextResponse> {
-  try {
-    const { name, email, password } = createUserSchema.parse(await req.json());
-
-    const hashed_password = await hash(password, 12);
-
-    const user = await prisma.user.create({
-      data: {
-        name,
-        email: email.toLowerCase(),
-        password: hashed_password,
-      },
-    });
-    return NextResponse.json({
-      user: {
-        name: user.name,
-        email: user.email,
-      },
-    });
-  } catch (error: any) {
-    if (error instanceof ZodError) {
-      return NextResponse.json(
-        {
-          status: "error",
-          message: "Validation failed",
-          errors: error.errors,
-        },
-        { status: 400 }
-      );
-    }
-    if (error.code === "P2002") {
-      return NextResponse.json(
-        {
-          status: "fail",
-          message: "user with that email already exists",
-        },
-        { status: 409 }
-      );
-    }
-    return NextResponse.json(
-      {
-        status: "error",
-        message: error.message || "Internal Server Error",
-      },
-      { status: 500 }
-    );
-  }
-}
-*/
+};
