fb57b2daff90032bbebd168f54137a7ff8642bd0
TLRKiliann <philogenie@protonmail.com>
2024-07-23 23:27:41 +0200

    design improved

--- my-app/components/products-and-cart/Card.tsx
+++ my-app/components/products-and-cart/Card.tsx
@@ -10,12 +10,12 @@ type ProductProps = {
 
 export default function Card({product}: ProductProps) {
     return (
-        <div key={product.id} className="w-full h-full text-slate-100/90 bg-gradient-to-tr from-slate-700 to-slate-950 rounded-md">
+        <div key={product.id} className="w-full h-full text-slate-100/90 bg-gradient-to-tr from-slate-700 to-slate-950 rounded-md shadow-card">
             
             <div className='w-full h-full flex flex-col items-center justify-between rounded-md'>
 
                 <div className="w-full h-auto flex flex-row items-start justify-between rounded-tl-tr-md">
-                    <div className='w-[100px] h-auto rounded-tl-md rounded-br-md'>
+                    <div className='w-[200px] h-auto rounded-tl-md rounded-br-md'>
                         <Image src={product.img}
                             width={100}
                             height={100}
@@ -24,18 +24,16 @@ export default function Card({product}: ProductProps) {
                         />
                     </div>
 
-                    <div className='flex items-center justify-start w-full pt-2'>
-                        <div className='flex flex-col items-start justify-start w-full'>
-                            <h3 className='text-xl'>{product.family}</h3>
-                            <h4 className='text-lg'>{product.name}</h4>
-                            <h4 className='text-xs my-2'>Version: {product.version}</h4>
-                            <p className='text-base font-bold'>{product.price}.-</p>
-                        </div>
+                    <div className='flex flex-col items-start justify-between w-full h-full pt-2 pl-[2%]'>
+                        <h3 className='text-xl'>{product.family}</h3>
+                        <h4 className='text-lg'>{product.name}</h4>
+                        <h4 className='text-xs my-2'>Version: {product.version}</h4>
+                        <p className='text-base font-bold'>{product.price}.-</p>
                     </div>
                 </div>
 
                 <div className='w-full'>
-                    <div className='flex items-center justify-center mt-4'>
+                    <div className='flex items-center justify-center'>
                         <MainBtnAdd
                             id={product.id}
                             name={product.name}
--- my-app/components/products-and-cart/HeaderProducts.tsx
+++ my-app/components/products-and-cart/HeaderProducts.tsx
@@ -35,32 +35,25 @@ const HeaderProducts = async () => {
     };
 
     return (
-        <div className="fixed z-20 top-0 w-full h-20 text-slate-500 bg-white border-none 
-            transition duration-300 ease-in-out opacity-100 shadow-md">
+        <div className="fixed z-20 top-0 w-full h-[10vh] text-lg font-bold text-slate-600/70 bg-white border-none shadow-md">
             <nav className="w-full h-full">
                 <ul className='w-full h-full flex flex-row items-center justify-end'>
 
-                    <li className='hover:text-blue-500 active:text-blue-600 mr-6'>
+                    <li className='transform duration-200 ease-in-out hover:text-gray-600/80 active:text-gray-400 mr-6'>
                         <Link href="/">Home</Link>
                     </li>
 
-                    <li className='hover:text-blue-500 active:text-blue-600 mr-6'>
+                    <li className='transform duration-200 ease-in-out hover:text-gray-600/80 active:text-gray-400 mr-6'>
                         <Link href="/products">Shop</Link>
                     </li>
 
-                    <li className='hover:text-blue-500 active:text-blue-600 mr-6'>
+                    <li className='transform duration-200 ease-in-out hover:text-gray-600/80 active:text-gray-400 mr-4'>
                         <Link href="/contact">Contact</Link>
                     </li>
 
-                    {!user && (
-                        <li className='hover:text-blue-500 active:text-blue-600 mr-4'>
-                            <Link href="/login">Login</Link>
-                        </li>
-                    )}
-
                     <li className='relative flex items-center justify-center w-[40px] h-[40px] transform 
-                        duration-200 ease-in-out hover:text-white hover:bg-blue-200 active:text-slate-200 
-                        active:bg-blue-300 rounded-full mr-6'>
+                        duration-200 ease-in-out hover:text-white hover:bg-blue-200 
+                        active:bg-blue-400 rounded-full mr-6'>
                         <Link href="/products/cart">
                             <FaShoppingCart size={18}/>
                             <CartItemsQuantity />
@@ -68,8 +61,8 @@ const HeaderProducts = async () => {
                     </li>
 
                     {user && (
-                        <form action={logoutAction}>
-                            <li className='flex items-center hover:text-blue-500 active:text-blue-600 mr-8'>
+                        <form title="logout" action={logoutAction}>
+                            <li className='flex items-center transform duration-200 ease-in-out hover:text-gray-600/80 active:text-gray-400 mr-8'>
                                 <button><FaPowerOff size={16} /></button>
                             </li>
                         </form>
