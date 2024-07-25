64fdaa61671fde37e400b349af87e146a4a2a7f7
TLRKiliann <philogenie@protonmail.com>
2024-07-25 21:45:11 +0200

    deleted

--- my-app/app/api/databrowser/route.ts
+++
@@ -1,5 +0,0 @@
-import filenameBrowser from '@/utils/browseros-data.json';
-
-export async function GET() {
-    return Response.json(filenameBrowser)
-}
--- my-app/app/api/dataip/route.ts
+++
@@ -1,5 +0,0 @@
-import filenameIp from '@/utils/ip-data.json';
-
-export async function GET() {
-    return Response.json(filenameIp)
-}
