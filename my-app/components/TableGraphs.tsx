a2eb75bfcab36113b9f4b20427ce1a7befa02ba9
TLRKiliann <philogenie@protonmail.com>
2024-07-26 16:39:42 +0200

    deleted

--- my-app/components/TableGraphs.tsx
+++
@@ -1,9 +0,0 @@
-export default function TableGraphs({children}: {children: React.ReactNode}) {
-    return (
-        <div className='w-full h-full bg-slate-800 border border-slate-700/30'>
-            <div className='flex items-center justify-center w-full h-full'>
-                {children}
-            </div>
-        </div>
-    )
-}
