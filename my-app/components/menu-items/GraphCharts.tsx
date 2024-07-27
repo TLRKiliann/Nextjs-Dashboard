6fd4b68724c04f7772e7181df8a8c923f86df9e4
TLRKiliann <philogenie@protonmail.com>
2024-07-27 13:04:28 +0200

    some changes for daily traffic component

--- my-app/components/menu-items/GraphCharts.tsx
+++ my-app/components/menu-items/GraphCharts.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import TableGraphs from './TableGraphs';
-import LineChart from './graphs/LineChart';
 import BarChartOs from './graphs/BarChartOs';
 import BarChartBrowser from './graphs/BarChartBrowser';
 import DoughnutChart from './graphs/DoughnutChart';
+import DailyTraffic from './graphs/daily-traffic';
 
 export default function GraphCharts() {
     return (
         <div className='w-full h-full grid grid-cols-2 grid-rows-2'>
 
             <TableGraphs>
-                <LineChart />
+                <DailyTraffic />
             </TableGraphs>
 
             <TableGraphs>
