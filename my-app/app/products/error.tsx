ae675846c84deab1c355dc497c065995d68bb94c
TLRKiliann <philogenie@protonmail.com>
2024-07-28 00:13:03 +0200

    created

---
+++ my-app/app/products/error.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+export default function ErrorBoundary({
+    error,
+    reset }: {
+    error: Error & { digest?: string }
+    reset: () => void }) {
+
+    // for router.back()
+    const router = useRouter();
+
+    return (
+        <div>
+            <h1 className='text-xl font-bold text-red-400'>Error: {error.message}</h1>
+            <button 
+                type="button" 
+                onClick={() => router.back()} 
+                className="bg-blue-600 mt-4 px-4 py-2 rounded"
+            >
+                Back to products
+            </button>
+        </div>
+    )
+}
