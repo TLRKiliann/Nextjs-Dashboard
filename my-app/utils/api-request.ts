de94ae3a0b8852ca78eafce8ab090091e2a6b302
TLRKiliann <philogenie@protonmail.com>
2024-07-27 19:05:32 +0200

    new api call created

--- my-app/utils/api-request.ts
+++ my-app/utils/api-request.ts
@@ -1,14 +1,16 @@
 import type { GeoLocationData } from "@/lib/definitions";
+import { Product } from "@prisma/client";
+
 
 export const ApiPublicIp = async (): Promise<{data: {ip: string;}}> => {
     try {
         const response = await fetch("https://jsonip.com/");
         if (!response.ok) {
-            throw new Error('Failed to fetch geolocation');
+            throw new Error('Failed to fetch public ip');
         }
         const data = await response.json();
         return { data: { ip: data.ip } };
-    } catch (error: any) {
+    } catch (error: unknown) {
         console.error('Error fetching public IP:', error);
         throw error;
     }
@@ -23,12 +25,26 @@ export const ApiGeolocation = async ({data}: {data: {ip: string;}}): Promise<Geo
             }
         });
         if (!geoRes.ok) {
-            throw new Error('Failed to fetch geolocation');
+            throw new Error('Error: geoRes.ok failed (GeoLocationData API call)!');
         }
         const geoData = await geoRes.json();
         return geoData;
-    } catch (error: any) {
+    } catch (error: unknown) {
         console.error('Error fetching geo IP:', error);
+        throw error;
+    }
+};
+
+export const fetchDataFromApi = async () => {
+    try {
+        const response = await fetch("/api/products");
+        if (!response.ok) {
+            throw new Error('Error: response.ok failed (products API call)!');
+        }
+        const products = (await response.json()) as Product[]
+        return products;
+    } catch (error: unknown) {
+        console.log('Failed to fetch products from API!', error);
         throw error;
     }
 };
