25261741e659ea7a63f02408652c3a4580d19197
TLRKiliann <philogenie@protonmail.com>
2024-07-28 23:23:49 +0200

    useQuery removed (not recommanded with nextjs)

--- my-app/app/products/page.tsx
+++ my-app/app/products/page.tsx
@@ -1,13 +1,12 @@
 import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
 import AllProducts from '@/components/AllProducts';
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import { fetchDataFromApi } from '@/utils/api-request';
-
-const queryClient = new QueryClient();
+import Loader from '@/components/Loader';
+import { Suspense } from 'react';
 
 export default async function ProductsPage() {
-  
+
   const session = await auth();
   const userSession = session?.user;
   
@@ -15,14 +14,11 @@ export default async function ProductsPage() {
     return redirect("/api/auth/signin");
   };
 
-  await queryClient.prefetchQuery({
-    queryKey: ["products"],
-    queryFn: fetchDataFromApi,
-  });
+  const products = await fetchDataFromApi();
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <AllProducts />
-    </HydrationBoundary>
+    <Suspense fallback={<Loader />}>
+      <AllProducts products={products} />
+    </Suspense>
   )
 };
