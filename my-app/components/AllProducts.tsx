142a4acd4993dad8830743afe08438de27014a38
TLRKiliann <philogenie@protonmail.com>
2024-07-29 17:44:25 +0200

    code review to combine prisma with store (zustand)

--- my-app/components/AllProducts.tsx
+++ my-app/components/AllProducts.tsx
@@ -5,20 +5,27 @@ import StoreOfProducts from '@/components/products-and-cart/store-of-products';
 import Loader from '@/components/Loader';
 import usePersistStore from '@/helpers/usePersistStore';
 import { useStore } from '@/stores/store';
+import { useEffect } from 'react';
 
-export default function AllProducts({ products }: {products: Product[]}): JSX.Element {
-    
+export default function AllProducts({ products }: { products: Product[] }): JSX.Element {
+
     // zustand
     const store = usePersistStore(useStore, (state) => state);
-    
+
+    useEffect(() => {
+        if (store && products) {
+            const currentProducts = store.bearProducts;
+            if ((JSON.stringify(currentProducts) !== JSON.stringify(products))) {
+                console.log("Setting products in store:", products);
+                store.setProducts(products);
+            }
+        }
+        return () => console.log("clean-up!");
+    }, [products, store]);
+
     if (!store || !products) {
         return <Loader />
     };
-
-    if (products && store) {
-        store.bearProducts = products;
-    };
-
     return (
         <div className='min-h-screen grid grid-cols-3 xl:grid-cols-4 grid-rows-3 bg-slate-100 gap-4 p-4 pt-[12vh]'>
             {store.bearProducts.map((product: Product) => (
