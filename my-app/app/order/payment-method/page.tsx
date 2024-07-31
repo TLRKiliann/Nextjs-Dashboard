a0964e5a663806f445f6959da559f9f80eed54ac
TLRKiliann <philogenie@protonmail.com>
2024-07-31 16:43:23 +0200

    renamed

---
+++ my-app/app/order/payment-method/page.tsx
@@ -0,0 +1,148 @@
+"use client";
+
+import { recordMethod } from '@/lib/actions';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
+import toast from 'react-hot-toast';
+
+type MethodProps = {
+    paypalMethod: boolean;
+    stripeMethod: boolean;
+    onDeliveryMethod: boolean;
+}
+
+export default function PaymentMethodPage() {
+
+    const router = useRouter();
+
+    const [paymentMethod, setPaymentMethod] = useState<MethodProps>({
+        paypalMethod: false,
+        stripeMethod: false,
+        onDeliveryMethod: false,
+    });
+
+    const [pathMethod, setPathMethod] = useState<string>("");
+
+    const handlePaypal = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPaymentMethod({
+            paypalMethod: event.currentTarget.checked,
+            stripeMethod: false,
+            onDeliveryMethod: false
+        })
+        setPathMethod("Paypal");
+    };
+
+    const handleStripe = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPaymentMethod({
+            paypalMethod: false,
+            stripeMethod: event.currentTarget.checked,
+            onDeliveryMethod: false
+        })
+        setPathMethod("Stripe");
+    };
+
+    const handleDelivery = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPaymentMethod({
+            paypalMethod: false,
+            stripeMethod: false,
+            onDeliveryMethod: event.currentTarget.checked,
+        })
+        setPathMethod("On_Delivery");
+    };
+
+    const onSubmit = async (pathMethod: string) => {
+        const response = await recordMethod(pathMethod);
+        if (response.message === "Payment Success!") {
+            toast.success("Payment method done!");
+            router.push("/order/payment-method/payment")
+        } else if (response.message === "Error with payment method!") {
+            toast.error("Payment method failed!")
+        } else {
+            toast.error("Error with payment method");
+        }    
+    };
+
+    return (
+        <div 
+            className='flex flex-col items-center justify-center w-full min-h-screen text-slate-800 bg-slate-100'>
+
+            <form
+                action={() => onSubmit(pathMethod)} 
+                className='flex flex-col items-start justify-between w-[360px] h-full bg-white px-4 py-6 
+                rounded shadow-lg'>
+
+                <div className='flex items-center w-full h-[40px] mb-4 pl-4'>
+                    <h1 className='text-lg font-bold text-slate-500'>
+                        Payment Method
+                    </h1>
+                </div>
+
+                <div className='w-4/5 h-[60%] flex flex-col items-start text-slate-700 bg-slate-100 rounded ml-8 p-4'>
+                    
+                    <div className='flex flex-row items-center justify-sart'>
+                        <label htmlFor="paypal">
+                            <input 
+                                type="radio" 
+                                id="paypal" 
+                                value="option1" 
+                                checked={paymentMethod.paypalMethod}
+                                onChange={(e) => handlePaypal(e)}
+                                className='mr-5'
+                            />
+                            Paypal
+                        </label>
+                    </div>
+
+                    <div className='flex flex-row items-center justify-sart'>
+
+                        <label htmlFor="stripe" className='pt-1'>
+                            <input
+                                type="radio"
+                                id="stripe"
+                                value="option2"
+                                checked={paymentMethod.stripeMethod}
+                                onChange={handleStripe}
+                                className='mr-5'
+                            />
+                                Stripe
+                        </label>
+                    </div>
+
+                    <div className='flex flex-row items-center justify-sart'>
+                        <label htmlFor="ondelivery" className='pt-1'>
+                            <input
+                                type="radio"
+                                id="ondelivery"
+                                value="option3"
+                                checked={paymentMethod.onDeliveryMethod} 
+                                onChange={handleDelivery}
+                                className='mr-5' 
+                            />
+                            Cash One Delivery
+                        </label>
+                    </div>
+                 
+                </div>    
+
+                <div className='w-full h-[20%] flex flex-row items-center justify-around mt-8 pb-2'>
+                    <button
+                        type="button"
+                        onClick={() => router.back()}
+                        className='text-base text-slate-50 font-bold bg-blue-500 
+                            hover:bg-blue-600 active:bg-blue-700
+                            px-6 py-[5px] rounded shadow-lg'>
+                        Back
+                    </button>
+                    <button 
+                        type="submit"
+                        disabled={!pathMethod ? true : false}
+                        className={`${!pathMethod ? "opacity-50" : "opacity-100"} text-base text-slate-50 
+                            font-bold bg-blue-500 hover:bg-blue-600 active:bg-blue-700 px-6 py-[5px] 
+                            rounded shadow-lg`}>
+                        Next
+                    </button>
+                </div>
+            </form>
+        </div>
+    )
+}
---
+++ my-app/app/order/payment-method/payment/page.tsx
@@ -0,0 +1,99 @@
+import { auth } from '@/auth';
+import prisma from '@/prisma/prisma';
+import { redirect } from 'next/navigation';
+import Link from 'next/link';
+import OrderItems from '@/components/order/order-items';
+import OrderSummary from '@/components/order/order-summary';
+import OrderShipping from '@/components/order/order-shipping';
+import OrderPayment from '@/components/order/order-payment';
+
+export default async function OrderPage() {
+
+    const session = await auth();
+    const user = session?.user;
+
+    if (!user?.id) {
+        return redirect("/api/auth/signin");
+    };
+
+    const paymentMethod = await prisma.user.findUnique({
+        where: {
+            id: user.id
+        },
+        include: {
+            payments: {
+                select: {
+                    usernameId: true,
+                    method: true
+                }
+            }
+        }
+    });
+
+    if (!paymentMethod?.payments.map((meth) => meth.method)) {
+        throw new Error("Error: payment method fetch failed!");
+    }
+
+    const findMethod = paymentMethod?.payments.find((meth) => meth.usernameId === user.id ? {...meth, method: meth.method}: meth);
+    
+    if (findMethod) {
+        console.log(findMethod, "findMethod")
+    } else {
+        throw new Error("Error: no method")
+    };
+
+    return (
+        <div className='w-full min-h-screen text-slate-700 bg-slate-50 p-4'>
+            
+            <div className='px-2 py-4'>
+                <h1 className='text-xl font-bold'>Place Order</h1>
+            </div>
+
+            <div className='grid grid-cols-2 grid-flow-row h-auto mx-2'>
+
+
+                <div className='flex flex-col w-full m-auto bg-white'>
+                    
+                    <div className='flex flex-col items-start justify-evenly w-full h-full p-4'>
+
+                        <OrderShipping />
+
+                    </div>
+
+                    <hr className='w-4/5 m-auto' />
+
+                    <div className='flex flex-col items-start justify-evenly w-full h-full p-4'>
+
+                        <OrderPayment findMethod={findMethod} />
+
+                    </div>
+
+                    <hr className='w-4/5 m-auto' />
+
+                    <div className='flex flex-col items-start justify-between w-full h-full p-4'>
+
+                        <OrderSummary />
+
+                    </div>
+                </div>
+ 
+                <div className='flex flex-col items-center justify-center bg-white w-full h-full m-auto'>
+                
+                    <div className='w-full h-full p-4 rounded'>
+
+                        <OrderItems />
+
+                    </div>
+
+                    <div className='flex flex-col items-center justify-center w-[92%]'>
+                        <Link href={`/order/payment-method/payment/${findMethod.method}`}
+                            className='w-full text-base text-center font-bold text-slate-50 bg-blue-500 
+                            hover:bg-blue-600 active:bg-blue-700 transition-colors py-2 rounded'>
+                            Place Order
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
