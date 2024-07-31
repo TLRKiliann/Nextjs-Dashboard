c23f4f15851814420bfca48addc044d982d89869
TLRKiliann <philogenie@protonmail.com>
2024-07-31 12:07:30 +0200

    name changed

---
+++ my-app/lib/functions.ts
@@ -0,0 +1,47 @@
+import prisma from "@/prisma/prisma";
+import { Connection, Message } from "@prisma/client";
+
+export const getConnections = async (startDate: Date, endDate: Date): Promise<Connection[]> => {
+    try {
+        const connections = await prisma.connection.findMany({
+            where: {
+                createdAt: {
+                    gte: new Date(startDate),
+                    lte: new Date(endDate),
+                }
+            }
+        });
+    
+        if (connections.length === 0) {
+            throw new Error("Error: prisma connection failed!");
+        };
+
+        return connections;
+    } catch (error) {
+        console.log("Error: connections fetch failed (getConnections())", error);
+        throw new Error("Error: connections fetch failed (getConnections())");
+    }
+};
+
+export const getMessages = async (startDate: Date, endDate: Date): Promise<Message[]> => {
+    try {
+        const messages = await prisma.message.findMany({
+            where: {
+                createdAt: {
+                    gte: new Date(startDate),
+                    lte: new Date(endDate),
+                }
+            }
+        });
+        console.log(messages);
+    
+        if (messages.length === 0) {
+            console.log("no message retrived");
+        };
+
+        return messages;
+    } catch (error) {
+        console.log("Error: connections fetch failed (getMessages())", error);
+        throw new Error("Error: connections fetch failed (getMessages())");
+    }
+};
