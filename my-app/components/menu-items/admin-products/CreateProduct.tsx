c51252f6e61bfcd6d3ca1b8f781a829138cdc3c6
TLRKiliann <philogenie@protonmail.com>
2024-08-05 14:31:39 +0200

    id incrementation for new product done

--- my-app/components/menu-items/admin-products/CreateProduct.tsx
+++ my-app/components/menu-items/admin-products/CreateProduct.tsx
@@ -1,3 +1,5 @@
+import type { Product } from "@prisma/client";
+import prisma from "@/prisma/prisma";
 import FormCreateContent from "./create-modify-form-content/form-create-content";
 
 /*
@@ -6,12 +8,25 @@ import FormCreateContent from "./create-modify-form-content/form-create-content"
     such as context with cart.
 */
 
-export default function CreateProduct() {
+export default async function CreateProduct() {
+
+    const nbOfProducts: Product[] = await prisma.product.findMany({
+        orderBy: {
+            id: "asc"
+        }
+    });
+
+    if (nbOfProducts.length === 0) {
+        return <div><h3>No products in db!</h3></div>
+    };
+
+    const idOfProduct: number = nbOfProducts.length + 1;
+
     return (
         <div className='relative z-50 flex flex-col items-center justify-center w-full h-[50%] bg-slate-800 rounded'>
             <div className='h-full flex flex-col items-center justify-center mx-4'>
                 <div className='flex flex-col items-center justify-center w-[460px] h-full rounded-lg'>
-                    <FormCreateContent />
+                    <FormCreateContent idOfProduct={idOfProduct} />
                 </div>
             </div>
         </div>
