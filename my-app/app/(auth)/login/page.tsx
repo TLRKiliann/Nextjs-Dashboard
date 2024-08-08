deabf8f51a1ba64170283f0ba0e16d3f38b6d704
TLRKiliann <philogenie@protonmail.com>
2024-08-08 18:34:27 +0200

    some changes

--- my-app/app/(auth)/login/page.tsx
+++ my-app/app/(auth)/login/page.tsx
@@ -21,7 +21,7 @@ export default async function LoginPage() {
                         <Link href="/register">Don&apos;t have an account ?</Link>
                     </li>
                     <li className="list-none text-sm text-blue-600/80 hover:text-blue-700/80 active:text-blue-800 mt-2">
-                        <Link href="/resetpassword">Forgot password ?</Link>
+                        <Link href="/resetpassword">Reset password ?</Link>
                     </li>
                 </div>
             </div>        
