779fd7523b59d6e9f36e8bbcd1cba845bc9bac93
TLRKiliann <philogenie@protonmail.com>
2024-08-09 12:18:17 +0200

    design improved

--- my-app/components/menu-items/bilan-components/bilan-content-box.tsx
+++ my-app/components/menu-items/bilan-components/bilan-content-box.tsx
@@ -14,24 +14,24 @@ export default function BilanContentBox(
         str_1, str_2, str_3, str_4, value_1, value_2, value_3, value_4 
     }: BoxValuesTypes) {
     return (
-        <div className='flex flex-col justify-between w-full h-full bg-white p-3 2xl:p-6 rounded shadow-sm-out'>
+        <div className='flex flex-col justify-between w-full h-full bg-cyan-950/70 p-2 rounded shadow-out'>
             
-            <div className="flex flex-row items-center justify-center bg-slate-100/70 border border-slate-50 px-2 py-1 rounded">
+            <div className="flex flex-row items-center justify-center bg-gradient-to-bl from-slate-900 to-cyan-900 border-none px-2 py-1 rounded shadow-medium-darker">
                 <p className='w-[65%] text-sm lg:text-base xl:text-lg'>{str_1}:</p>
                 <p className='flex justify-end w-[35%] text-sm lg:text-base xl:text-lg'>&nbsp;{value_1}</p>
             </div>
 
-            <div className="flex flex-row items-center justify-center bg-slate-100/70 border border-slate-50 px-2 py-1 rounded">
+            <div className="flex flex-row items-center justify-center bg-gradient-to-bl from-slate-900 to-cyan-900 border-none px-2 py-1 rounded shadow-medium-darker">
                 <p className='w-[65%] text-sm lg:text-base xl:text-lg'>{str_2}:</p>
                 <p className='flex justify-end w-[35%] text-sm lg:text-base xl:text-lg'>&nbsp;{value_2}</p>
             </div>
 
-            <div className="flex flex-row items-center justify-center bg-slate-100/70 border border-slate-50 px-2 py-1 rounded">
+            <div className="flex flex-row items-center justify-center bg-gradient-to-bl from-slate-900 to-cyan-900 border-none px-2 py-1 rounded shadow-medium-darker">
                 <p className='w-[65%] text-sm lg:text-base xl:text-lg'>{str_3}:</p>
                 <p className='flex justify-end w-[35%] text-sm lg:text-base xl:text-lg'>&nbsp;{value_3}</p>
             </div>
 
-            <div className="flex flex-row items-center justify-center bg-slate-100/70 border border-slate-50 px-2 py-1 rounded">
+            <div className="flex flex-row items-center justify-center bg-gradient-to-bl from-slate-900 to-cyan-900 border-none px-2 py-1 rounded shadow-medium-darker">
                 <p className='w-[65%] text-sm lg:text-base xl:text-lg'>{str_4}:</p>
                 <p className='flex justify-end w-[35%] text-sm lg:text-base xl:text-lg'>&nbsp;{value_4}</p>
             </div>
