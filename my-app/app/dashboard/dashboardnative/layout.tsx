1ddce11b1cdb41d2fc292bd9df581f614abef737
TLRKiliann <philogenie@protonmail.com>
2024-08-10 13:11:12 +0200

    bg changed to white

--- my-app/app/dashboard/dashboardnative/layout.tsx
+++ my-app/app/dashboard/dashboardnative/layout.tsx
@@ -12,7 +12,7 @@ export default async function DashboardNativeLayout({children, tableone, squares
     squares: React.ReactNode;
 }) {
     return (
-        <div className='flex flex-col w-full min-h-screen bg-slate-100'>
+        <div className='flex flex-col w-full min-h-screen bg-white'>
 
             <div className='flex flex-row w-full h-[100vh]'>
 
@@ -26,12 +26,12 @@ export default async function DashboardNativeLayout({children, tableone, squares
 
                     <div className='flex items-center justify-evenly w-full h-[70%] pt-2 pb-6'>
 
-                        <div className='flex w-[45%] h-full text-slate-500/80 bg-slate-50 
+                        <div className='flex w-[45%] h-full text-slate-500/80 bg-white 
                             p-4 shadow-sm-out rounded-lg'>
                             {tableone}
                         </div>
 
-                        <div className='flex w-[45%] h-full text-slate-500/80 bg-slate-50 
+                        <div className='flex w-[45%] h-full text-slate-500/80 bg-white 
                             p-4 shadow-sm-out rounded-lg'>
                             {tabletwo}
                         </div>
