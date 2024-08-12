bc2f9b306c80b7c90a2f9b87b145ef79110164ab
TLRKiliann <philogenie@protonmail.com>
2024-08-12 12:07:23 +0200

    deleted

--- my-app/utils/generate-version.ts
+++
@@ -1,7 +0,0 @@
-function generateVersion(): string {
-    const min: number = 100000000;
-    const max: number = 999999999;
-    const version = String(Math.floor(Math.random() * (max - min)) + min
-    );
-    return version;
-};
