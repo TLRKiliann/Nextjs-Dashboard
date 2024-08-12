7f7b2c8d8cd0f3f1fc6b211180ae47a2f951448c
TLRKiliann <philogenie@protonmail.com>
2024-08-12 15:20:57 +0200

    productName replaced by productId

---
+++ my-app/app/products/[productId]/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+
+type ParamsProps = {
+    params: {
+        productId: string;
+    }
+};
+
+export const generateMetadata = async ({params}: ParamsProps): Promise<Metadata> => {
+    const title = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Prod: ${params.productId}`)
+        }, 300)
+    })
+    return {
+      title: `HW ${title}`
+    }
+};
+
+export default function ProductIdLayout({children}: {children: React.ReactNode}) {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+};
---
+++ my-app/app/products/[productId]/page.tsx
@@ -0,0 +1,17 @@
+import { Suspense } from 'react';
+import { notFound } from 'next/navigation';
+import ProductId from '@/components/products-and-cart/ProductId';
+import Loader from '@/components/Loader';
+
+export default async function ProductIdPage({ params }: {params: { productId: string }}) {
+
+    if (!params.productId) {
+        notFound();
+    };
+
+    return (
+        <Suspense fallback={<Loader />}>
+            <ProductId params={params} />
+        </Suspense>
+    )
+};
