2b616a1e1678524d5b0a62d97368b477ca8718f9
TLRKiliann <philogenie@protonmail.com>
2024-08-12 15:20:08 +0200

    route changed

---
+++ my-app/app/payment/[paymentId]/page.tsx
@@ -0,0 +1,10 @@
+export default function PaymentIdPage() {
+
+    return (
+        <div className="flex items-center justify-center w-full h-[80vh]">
+
+            <span className="text-5xl">➡️</span>
+
+        </div>
+    )
+}
