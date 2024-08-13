9cade91a7d63a91bad5b532a826eb563cf5888c9
TLRKiliann <philogenie@protonmail.com>
2024-08-13 22:16:34 +0200

    color of grid changed

--- my-app/components/menu-items/bilan-components/TotalSalesByProduct.tsx
+++ my-app/components/menu-items/bilan-components/TotalSalesByProduct.tsx
@@ -44,7 +44,7 @@ export const options = {
   scales: {
     x: {
       grid: {
-        color: 'rgba(255, 255, 255, 0.2)',
+        color: 'rgba(255, 255, 255, 0.1)',
       },
       ticks: {
         color: 'rgba(255, 255, 255, 0.8)',
@@ -55,7 +55,7 @@ export const options = {
     },
     y: {
       grid: {
-        color: 'rgba(255, 255, 255, 0.2)',
+        color: 'rgba(255, 255, 255, 0.1)',
       },
       ticks: {
         color: 'rgba(255, 255, 255, 0.8)',
