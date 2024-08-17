586bf0743af08d2065092a4e5e7577bf45a14540
TLRKiliann <philogenie@protonmail.com>
2024-08-17 16:48:39 +0200

    padding & gap improved

--- my-app/components/AllProducts.tsx
+++ my-app/components/AllProducts.tsx
@@ -10,7 +10,7 @@ export default function AllProducts({ products, user }: { products: Product[]; u
     };
 
     return (
-        <div className='min-h-screen grid grid-cols-3 xl:grid-cols-4 grid-rows-3 bg-slate-50 gap-4 p-4 pt-[12vh]'>
+        <div className='min-h-screen grid grid-cols-3 xl:grid-cols-4 grid-rows-3 bg-slate-50 gap-10 p-10 pt-[14vh]'>
             {products.map((product: Product) => (
                 <StoreOfProducts
                     key={product.id}
