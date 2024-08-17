5dfe4ca8f2dca587fad88cc40f58a30d259a5553
TLRKiliann <philogenie@protonmail.com>
2024-08-17 16:49:14 +0200

    style changed

--- my-app/components/products-and-cart/store-of-products.tsx
+++ my-app/components/products-and-cart/store-of-products.tsx
@@ -45,44 +45,36 @@ export default async function StoreOfProducts({product, user}: ProductProps) {
     };
 
     return (
-        <div key={product.id} className="w-full h-full text-slate-500 bg-white rounded-md shadow-xs-out">
+        <div key={product.id} className="w-full h-full text-slate-100 bg-gradient-to-bl from-slate-900 to-cyan-900 rounded-md shadow-xs-out">
             
             <div className='w-full h-full flex flex-col items-center justify-between rounded-md'>
 
-                <div className="w-full h-auto flex flex-row items-start justify-between rounded-tl-tr-md">
-                    
-                    <div className='w-[200px] h-auto rounded-tl-md rounded-br-md'>
-                        <Image src={product.img}
-                            width={100}
-                            height={100}
-                            className="object-cover rounded-tl-md rounded-br-md shadow-white" 
-                            alt="no img" 
-                        />
-                    </div>
-
-                    <div className='flex flex-col items-start justify-between w-full h-full pt-2 pl-[2%]'>
-                        <h3 className='text-xl'>{product.family}</h3>
-                        <h4 className='text-lg'>{product.name}</h4>
-                        <h4 className='text-xs my-2'>Version: {product.version}</h4>
-                        <p className='text-base font-bold'>{product.price}.-</p>
-                    </div>
-
+                <div className='flex items-center justify-center w-full h-[60%] bg-white rounded-tl-md rounded-tr-md'>
+                    <Image src={product.img}
+                        width={160}
+                        height={160}
+                        className="object-cover rounded-tl-md rounded-tr-md shadow-white" 
+                        alt="no img" 
+                    />
                 </div>
 
-                <div className='w-full'>
-                    <div className='flex items-center justify-center'>
-
-                        <MainBtnAdd
-                            id={product.id}
-                            name={product.name}
-                            storeQuantity={storeQuantity.carts}
-                            product={product}
-                        />
-
-                    </div>
+                <div className='flex flex-col items-center justify-between w-full h-[30%] my-4'>
+                    <h3 className='text-xl'>{product.family}</h3>
+                    <h4 className='text-lg'>{product.name}</h4>
+                    <h4 className='text-xs my-2'>Version: {product.version}</h4>
+                    <p className='text-base font-bold'>{product.price}.-</p>
                 </div>
 
-                <div className='text-center my-4'>
+                <div className='flex items-center justify-center w-full h-[10%]'>
+                    <MainBtnAdd
+                        id={product.id}
+                        name={product.name}
+                        storeQuantity={storeQuantity.carts}
+                        product={product}
+                    />
+                </div>
+
+                <div className='h-[10%] text-center my-4'>
                     <li className='list-none text-blue-400/80 hover:text-blue-500/80 
                         active:text-blue-600/80 transition-colors'>
                         <Link href={`/products/${product.name}`}>
