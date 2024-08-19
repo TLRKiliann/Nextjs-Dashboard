01ded1e63fe9dc3fb5432d80d0e3424097cb5abf
TLRKiliann <philogenie@protonmail.com>
2024-08-19 14:51:22 +0200

    name of file route.ts changed

---
+++ my-app/app/api/products/route.ts
@@ -0,0 +1,25 @@
+import { Product } from "@prisma/client";
+import prisma from "@/prisma/prisma";
+import { auth } from "@/auth";
+
+export async function GET() {
+    try {
+        const session = await auth();
+        const user = session?.user;
+        
+        if (!user) {
+            return Response.json({error: "Unauthorized"}, {status: 401});
+        };
+
+
+        const products: Product[] = await prisma.product.findMany({
+            orderBy: {
+                id: "asc",
+            }
+        });
+        return Response.json(products);
+    } catch (error) {
+        console.log(error);
+        return Response.json({error: "internal server error (products)"}, {status: 500});
+    }
+}
