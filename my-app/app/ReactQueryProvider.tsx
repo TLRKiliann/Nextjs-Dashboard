1bad6304638f56ebf5e7cb133ea996be3880b8f1
TLRKiliann <philogenie@protonmail.com>
2024-08-21 11:42:22 +0200

    RC devtools commented

--- my-app/app/ReactQueryProvider.tsx
+++ my-app/app/ReactQueryProvider.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { useState } from "react";
+//import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function ReactQueryProvider({children}: {children: React.ReactNode}) {
-    const [client] = useState(new QueryClient);
+    //const [queryClient] = useState(new QueryClient);
+    const queryClient = new QueryClient()
     return (
-        <QueryClientProvider client={client}>
+        <QueryClientProvider client={queryClient}>
             {children}
-            <ReactQueryDevtools initialIsOpen={false} />
+            {/* <ReactQueryDevtools initialIsOpen={false} /> */}
         </QueryClientProvider>
     )
 }
