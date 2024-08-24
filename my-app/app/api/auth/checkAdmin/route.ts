7d6f19ca24b9e8dd80c92bd9161984968e3ee85a
TLRKiliann <philogenie@protonmail.com>
2024-08-24 21:06:38 +0200

    git commit -m new route created to verify if user is admin

---
+++ my-app/app/api/auth/checkAdmin/route.ts
@@ -0,0 +1,23 @@
+// pages/api/checkAdmin.ts
+import { auth } from '@/auth';
+import prisma from '@/prisma/prisma';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+    const session = await auth();
+    const user = session?.user;
+
+
+    if (!user || !user?.email) {
+        return NextResponse.json({ isAdmin: false }, {status: 401});
+    };
+
+    const admin = await prisma.user.findUnique({
+        where: {
+            email: user.email,
+            role: "ADMIN"
+        }
+    });
+
+    return NextResponse.json({ isAdmin: !!admin }, {status: 200});
+};
