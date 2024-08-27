8f493f355f538cda3485766323dd4f0014e212f1
TLRKiliann <philogenie@protonmail.com>
2024-08-27 14:18:21 +0200

    style improved & new component created

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,24 +1,34 @@
 import Link from "next/link";
 import Image from 'next/image';
 import imgLand from '@/public/assets/images/bg/landscape-contact.jpg';
+import Slider from "@/components/Slider";
 
 export default function Home() {
-  //bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-slate-200 from-0% to-sky-500 to-70% 
-  //bg-gradient-to-br from-slate-50 to-sky-500
+  /*
+    bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-slate-200 from-0% to-sky-500 to-70% 
+    bg-gradient-to-br from-slate-50 from-10% via-sky-400 via-50% to-sky-500 to-90%
+    bg-gradient-to-br from-slate-50 to-sky-500
+    bg-gradient-to-br from-sky-200 to-blue-400/70
+  */
   return (
     <div className="absolute -z-20 w-full min-h-screen flex flex-col items-center justify-center 
       bg-gradient-to-br from-slate-50 to-sky-500 p-4">
 
+      <div className="absolute -z-30 top-0 right-0 w-full h-full">
+        <Slider />
+      </div>
 
-      <div className="absolute w-[650px] h-[390px] flex items-center justify-center bg-sky-200/70 opacity-60 shadow-in rounded">
-        <div className="absolute -z-10 flex items-center justify-center w-[500px] h-[260px] bg-purple-700 rounded shadow-sm-out">
+      <div className="absolute -z-20 w-[650px] h-[390px] flex items-center justify-center 
+        bg-gradient-to-br from-sky-200 to-sky-400 shadow-in rounded-lg">
+        <div className="absolute -z-10 flex items-center justify-center w-[500px] h-[240px] 
+          bg-gradient-to-tl from-purple-600/50 to-violet-500/30 rounded-lg shadow-sm-out">
           <Image
             src={imgLand}
             priority={true}
             width={1920}
             height={1080}
             alt="no img" 
-            className="fixed -z-10 w-[600px] h-auto opacity-70 object-cover rounded shadow-out"
+            className="fixed z-0 w-[600px] h-auto opacity-40 object-cover rounded shadow-out"
           />
         </div>
       </div>
@@ -27,7 +37,7 @@ export default function Home() {
         <h1 className="text-3xl font-bold text-slate-100">Dashboard - Project</h1>
       </div>
 
-      <div className="mt-16 w-[500px] h-auto flex flex-col items-center justify-cemter">
+      <div className="mt-[60px] w-[500px] h-auto flex flex-col items-center justify-cemter">
         
         <ul className="list-none">
 
