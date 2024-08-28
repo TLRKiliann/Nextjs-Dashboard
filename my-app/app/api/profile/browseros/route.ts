8d50c5872f29ef65ff8086fba7e71ea3d4ad9e8b
TLRKiliann <philogenie@protonmail.com>
2024-08-28 12:04:22 +0200

    code splitted (fs/promise is not supported by nextjs api routes)

--- my-app/app/api/profile/browseros/route.ts
+++ my-app/app/api/profile/browseros/route.ts
@@ -1,50 +1,50 @@
-//import { ApiPublicIp } from "@/utils/api-request";
-import { readFile, writeFile } from "fs/promises";
 import { NextResponse } from "next/server";
+import { readData, writeData } from '@/utils/api-request';
 
 export async function POST(request: Request) {
-    const body = await request.json();
-    if (body === null) {
-        return NextResponse.json({message: "browser failed"});
+    const body: string = await request.json();
+    if (!body) {
+        return NextResponse.json({ message: "browser failed" });
     };
-    const browser = body;
-    const filename = './utils/browseros-data.json';
+    const browser: string = body;
+    const filename: string = './utils/browseros-data.json';
     try {
-        const nextBrowser = { browser };
-    
-        const file = await readFile(filename, { encoding: 'utf8' });
-        const prevBrowsers = JSON.parse(file);
+        const nextBrowser: { browser: string } = { browser };
+
+        const prevBrowsers = await readData(filename) || [];
         prevBrowsers.push(nextBrowser);
 
-        await writeFile(filename, JSON.stringify(prevBrowsers, null, 4));
+        await writeData(filename, prevBrowsers);
         console.log('Data has been written to browseros-data.json');
     } catch (error) {
-        throw new Error('An error occurred while writing to browseros-data.json:');
+        throw new Error(`An error occurred while writing to browseros-data.json: ${error}`);
     };
 
     try {
-        const res = await fetch("http://localhost:3000/api/dashboard/publicip");
-        const ipResult = await res.json();
+        const apiUrl: string = `${process.env.NEXTAUTH_URL}/api/dashboard/publicip`;
+        
+        const res = await fetch(apiUrl);
+        const ipResult: {data: {ip: string}} = (await res.json()) as {data: {ip: string}};
+
         if (!ipResult) {
-            throw new Error("No ip public detected");
+            throw new Error("No public IP detected");
         } else {
             console.log("Public IP detected");
         };
-    
-        const filename = './utils/ip-data.json';
+        
+        const ipFilename: string = './utils/ip-data.json';
+        
         try {
-            const file = await readFile(filename, { encoding: 'utf8' });
-            const previousIp = JSON.parse(file);
-            previousIp.push(ipResult);
+            const file = await readData(ipFilename) || [];
+            file.push(ipResult);
 
-            await writeFile(filename, JSON.stringify(previousIp, null, 4));
+            await writeData(ipFilename, file);
             console.log('Data has been written to ip-data.json');
         } catch (err) {
-            throw new Error('An error occurred while writing to ip-data.json:');
+            throw new Error(`An error occurred while writing to ip-data.json: ${err}`);
         };
-
     } catch (error) {
-        throw new Error('An error occurred while writing to browseros-data.json:'); 
-    }
-    return NextResponse.json({message: "browser & os detected"});
-}
+        throw new Error(`An error occurred while fetching public IP: ${error}`); 
+    };
+    return NextResponse.json({ message: "browser & os detected" }, { status: 200 });
+};
