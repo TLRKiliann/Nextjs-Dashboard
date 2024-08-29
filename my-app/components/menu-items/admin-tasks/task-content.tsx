6fc2bb40308e0f7a1937e20877dd33812f0e8a19
TLRKiliann <philogenie@protonmail.com>
2024-08-29 21:31:20 +0200

    some changes

--- my-app/components/menu-items/admin-tasks/task-content.tsx
+++ my-app/components/menu-items/admin-tasks/task-content.tsx
@@ -101,6 +101,7 @@ export default function TasksContent(): JSX.Element {
             <h1 className='text-3xl font-bold text-blue-500/80'>
                 Tasks
             </h1>
+
             <div className='flex flex-row items-center justify-center w-4/5 bg-gradient-to-l from-orange-400 to-yellow-100 border border-orange-100 m-auto my-4 py-2 rounded-full'>
                 <input
                     type="text"
@@ -110,6 +111,7 @@ export default function TasksContent(): JSX.Element {
                     className="w-[300px] xl:w-[400px] text-base font-normal text-slate-700/70 bg-white border border-solid border-orange-300 transition ease-in-out
                         focus:text-slate-700/70 focus:bg-white focus:border-orange-400 focus:outline-none my-0 mr-4 px-3 py-2 rounded-full"
                 />
+                
                 <button
                     type="button"
                     onClick={handleClick}
@@ -128,10 +130,12 @@ export default function TasksContent(): JSX.Element {
                     {item.display === false ? (
                         <div 
                             key={item.id} 
-                            className='flex flex-row items-center justify-between w-full'>
+                            className='flex flex-row items-center justify-between w-full'
+                        >
                             <li className='list-none text-base text-slate-700/70'>
                                 <p>{item.task}</p>
                             </li>                        
+                        
                             <span
                                 onClick={() => handleModify(item.id)} 
                                 className='text-slate-50 bg-blue-500 cursor-pointer transition ease-in-out duration-100 hover:bg-blue-600 hover:text-slate-200 hover:scale-105
@@ -151,6 +155,7 @@ export default function TasksContent(): JSX.Element {
                                 className="w-[300px] xl:w-[400px] text-base font-normal text-slate-700/70 bg-white border border-solid border-orange-300 transition ease-in-out
                                     focus:text-slate-700/70 focus:bg-white focus:border-orange-400 focus:outline-none mr-4 px-3 py-2 rounded-md"
                             />
+                            
                             <span
                                 onClick={() => handleSave(item.id)} 
                                 className='text-slate-50 bg-blue-500 cursor-pointer transition ease-in-out duration-100 
