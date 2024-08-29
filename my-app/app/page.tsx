9a2ee8c71ddee1471471e40dedefbf37a17b681a
TLRKiliann <philogenie@protonmail.com>
2024-08-29 12:07:32 +0200

    design improved

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -6,7 +6,7 @@ import imgLand from '@/public/assets/images/bg/landscape-contact.jpg';
 export default function Home() {
   return (
     <div className="absolute -z-20 w-full min-h-screen flex flex-col items-center justify-center 
-      bg-gradient-to-br from-slate-50 to-sky-500 p-4">
+      text-slate-300 bg-gradient-to-br from-slate-50 to-sky-500 p-4">
 
       <div className="absolute -z-30 top-0 right-0 w-full h-full">
         <Carousel />
@@ -14,8 +14,8 @@ export default function Home() {
 
       <div className="absolute -z-20 w-[650px] h-[390px] flex items-center justify-center 
         bg-gradient-to-br from-sky-200 to-sky-400 shadow-in rounded-lg">
-        <div className="absolute -z-10 flex items-center justify-center w-[500px] h-[240px] 
-          bg-gradient-to-tl from-purple-600/50 to-violet-500/20 rounded-lg shadow-sm-out">
+        <div className="absolute -z-10 flex items-center justify-center w-[600px] h-[340px] 
+          bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-500/60 from-50% to-purple-500/5 to-70% rounded-lg">
           <Image
             src={imgLand}
             priority={true}
@@ -28,26 +28,26 @@ export default function Home() {
       </div>
 
       <div className="absolute h-[240px] pt-6 z-0 ">
-        <h1 className="text-3xl font-bold text-slate-100">Dashboard - Project</h1>
+        <h1 className="text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-br from-yellow-500 to-red-500 drop-shadow-text">Dashboard - Project</h1>
       </div>
 
       <div className="mt-[60px] w-[500px] h-auto flex flex-col items-center justify-cemter">
         
         <ul className="list-none">
 
-          <li className="transform duration-100 ease-in-out text-slate-100 hover:text-cyan-200 hover:scale-105 active:text-cyan-300 active:scale-95 py-1">
+          <li className="drop-shadow-text transform duration-100 ease-in-out hover:text-cyan-200 hover:scale-105 active:text-sky-400 active:scale-95 py-1">
             <Link href="/dashboard/dashboardnative">Dashboard (login as admin required)</Link>
           </li>
 
-          <li className="transform duration-100 ease-in-out text-slate-100 hover:text-cyan-200 hover:scale-105 active:text-cyan-300 active:scale-95 py-1">
+          <li className="drop-shadow-text transform duration-100 ease-in-out hover:text-cyan-200 hover:scale-105 active:text-sky-400 active:scale-95 py-1">
             <Link href="/products">Products (login as user required)</Link>
           </li>
 
-          <li className="transform duration-100 ease-in-out text-slate-100 hover:text-cyan-200 hover:scale-105 active:text-cyan-300 active:scale-95 py-1">
+          <li className="drop-shadow-text transform duration-100 ease-in-out hover:text-cyan-200 hover:scale-105 active:text-sky-400 active:scale-95 py-1">
             <Link href="/profile">Profile (login as user required)</Link>
           </li>
 
-          <li className="transform duration-100 ease-in-out text-orange-300 hover:text-orange-400 hover:scale-105 active:text-orange-500 active:scale-95 py-1">
+          <li className="drop-shadow-text transform duration-100 ease-in-out text-orange-300 hover:text-orange-400/70 hover:scale-105 active:text-orange-500 active:scale-95 py-1">
             <Link href="/products-list">Products list (login as user required)</Link>
           </li>
         
