ac24c11c40fbb4210a5f4f03160430cbef72e8e7
TLRKiliann <philogenie@protonmail.com>
2024-08-31 15:19:08 +0200

    code review to retrieve ip from client component and not from server

--- my-app/app/api/profile/browseros/route.ts
+++ my-app/app/api/profile/browseros/route.ts
@@ -3,12 +3,10 @@ import { NextResponse } from "next/server";
 import { readData, writeData } from '@/utils/api-request';
 
 export async function POST(request: Request) {
-    const body: string = await request.json();
-    if (!body) {
-        return NextResponse.json({ message: "browser failed" });
+    const body = await request.json();
+    if (!body || !body.browser || !body.ip) {
+        return NextResponse.json({ message: "method post failed" }, { status: 400 });
     };
-    const browser: string = body;
-    const filename: string = './utils/browseros-data.json';
 
     // authorization
     const session = await auth();
@@ -20,6 +18,13 @@ export async function POST(request: Request) {
 
     const username = user.name;
 
+    const browser: string = body.browser;
+    const filename: string = './utils/browseros-data.json';
+
+    if (!browser) {
+        return NextResponse.json({message: "browser is not defined"}, {status: 400});
+    };
+
     try {
         const nextBrowser: { browser: string; username: string } = { browser, username };
 
@@ -29,35 +34,33 @@ export async function POST(request: Request) {
         await writeData(filename, prevBrowsers);
         console.log('Data has been written to browseros-data.json');
     } catch (error) {
-        throw new Error(`An error occurred while writing to browseros-data.json: ${error}`);
+        return NextResponse.json({ error: `An error occurred with browseros-data.json: ${error}` }, { status: 500 });
     };
 
+    const dataIp: string = body.ip;
+    const ipFilename: string = './utils/ip-data.json';
+
+    if (!dataIp) {
+        return NextResponse.json({message: "dataIp is not defined"}, {status: 400});
+    };
+
+    const ipResult: {dataIp: string; username: string} = { dataIp, username };
+
+    if (!ipResult) {
+        throw new Error("No public IP detected");
+    } else {
+        console.log("Public IP detected");
+    };
+    
     try {
-        const apiUrl: string = `${process.env.NEXTAUTH_URL}/api/dashboard/publicip`;
-        
-        const res = await fetch(apiUrl);
-        const dataIpUser: { data: { ip: string; } } = (await res.json()) as { data: { ip: string; } };
-        const ipResult: { dataIpUser: { data: { ip: string; } }, username: string; } = { dataIpUser, username };
+        const file = await readData(ipFilename) || [];
+        file.push(ipResult);
 
-        if (!ipResult) {
-            throw new Error("No public IP detected");
-        } else {
-            console.log("Public IP detected");
-        };
-        
-        const ipFilename: string = './utils/ip-data.json';
-        
-        try {
-            const file = await readData(ipFilename) || [];
-            file.push(ipResult);
-
-            await writeData(ipFilename, file);
-            console.log('Data has been written to ip-data.json');
-        } catch (err) {
-            throw new Error(`An error occurred while writing to ip-data.json: ${err}`);
-        };
-    } catch (error) {
-        throw new Error(`An error occurred while fetching public IP: ${error}`); 
+        await writeData(ipFilename, file);
+        console.log('Data has been written to ip-data.json');
+    } catch (err) {
+        return NextResponse.json({ error: `An error occurred with ip-data.json: ${err}` }, { status: 500 });
     };
+
     return NextResponse.json({ message: "browser & os detected" }, { status: 200 });
 };
